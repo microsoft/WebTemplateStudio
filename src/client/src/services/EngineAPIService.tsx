3d6d13b879e69df3cc089d5d640d9391a9b0b741
Amr Sharaf <t-amrsha@microsoft.com>
2019-02-07 15:59:48 -0800

    Updated endpoints

--- src/client/src/services/EngineAPIService.tsx
+++ src/client/src/services/EngineAPIService.tsx
@@ -1,41 +1,88 @@
 import fetch from "node-fetch";
 
-const API = "http://localhost:";
-
 export class EngineAPIService {
-    private port: string;
 
-    constructor(port: string) {
-        this.port = port;
+    private API: string;
+
+    constructor(port: string, host: string | undefined) {
+        if (!undefined) {
+            this.API = "http://localhost:" + port;
+        } else {
+            this.API = host + ":" + port;
+        }
     }
 
-    public async getProjectTypes() {
-        return fetch(API + this.port + "/api/projectType")
-            .then((response) => response.json())
+    public async getProjectTypes(platform: string, language: string) {
+        const url = new URLSearchParams(this.API + "/api/projectType");
+        url.append("platform", platform);
+        url.append("language", language);
+        fetch(url.toString(), { method: "get" })
+            .then((response: Response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    return {};
+                }
+
+            })
             .catch((error: Error) => {
                 return {};
             });
     }
 
-    public async getAllFrameworks(projectType: string) {
-        return fetch(API + this.port + "/api/framework?projectType=" + projectType)
-            .then((response) => response.json())
+    public async getFrameworks(projectType: string) {
+        const url = new URLSearchParams(this.API + "/api/framework");
+        url.append("projectType", projectType);
+        fetch(url.toString(), { method: "get" })
+            .then((response: Response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    return {};
+                }
+
+            })
             .catch((error: Error) => {
                 return {};
             });
     }
 
-    public async getFrontendFrameworks(projectType: string) {
-        return fetch(API + this.port + "/api/framework/frontend?projectType=" + projectType)
-            .then((response) => response.json())
-            .catch((error: Error) => {
+    public async syncPlatforms(platform: string) {
+        throw Error("Undefined function");
+    }
+
+    public async getFeatures(frameworks: [string]) {
+        const url = new URLSearchParams(this.API + "/api/feature");
+        frameworks.forEach((framework) => {
+            url.append("framework", framework);
+        });
+        fetch(url.toString(), { method: "get" })
+            .then((response: Response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    return {};
+                }
+
+            }).catch((error: Error) => {
                 return {};
             });
     }
 
-    public async getBackendFrameworks(projectType: string) {
-        return fetch(API + this.port + "/api/framework/backend?projectType=" + projectType)
-            .then((response) => response.json())
+    public async getPages(frameworks: [string]) {
+        const url = new URLSearchParams(this.API + "/api/page");
+        frameworks.forEach((framework) => {
+            url.append("framework", framework);
+        });
+        fetch(url.toString(), { method: "get" })
+            .then((response: Response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    return {};
+                }
+
+            })
             .catch((error: Error) => {
                 return {};
             });
