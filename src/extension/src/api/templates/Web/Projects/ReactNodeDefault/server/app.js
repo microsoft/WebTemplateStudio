2ad3358eae14657529ce5253e6ff13e739b6d88e
Sahil Tara <t-satara@microsoft.com>
2019-02-25 08:37:21 -0800

    Updated templates in both src/api and root folder

---
+++ src/extension/src/api/templates/Web/Projects/ReactNodeDefault/server/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const index = require('./routes/index');
+
+const app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'build')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use('/api', index);
+app.get('*', (req, res) => {
+  res.sendFile('build/index.html', { root: global });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ templates/Web/Projects/ReactNodeDefault/server/constants.js
@@ -0,0 +1,5 @@
+var constants = {};
+
+constants.port = process.env.PORT || "3001";
+
+module.exports = constants;
