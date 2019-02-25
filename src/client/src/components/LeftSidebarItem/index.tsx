054e81b95a7b48a10a8f61a2bdc7f1d6b224812c
Kelly Ng <t-keng@microsoft.com>
2019-02-25 11:25:12 -0800

    fix svg import naming

--- src/client/src/components/LeftSidebarItem/index.tsx
+++ src/client/src/components/LeftSidebarItem/index.tsx
@@ -1,17 +1,26 @@
 import * as React from "react";
 
-import Check from "../../assets/Check.svg";
+import Check from "../../assets/check.svg";
 
 import styles from "./styles.module.css";
 
-const SidebarItem = ({ text, showCheck }: { text: string, showCheck: boolean }) => {
+const SidebarItem = ({
+  text,
+  showCheck
+}: {
+  text: string;
+  showCheck: boolean;
+}) => {
   return (
     <div className={styles.container}>
-      {showCheck ? <img src={Check} className={styles.icon} /> : <div className={styles.spacer}/>}
-      <div className={styles.text}>
-        {text}
-      </div>
-    </div>);
+      {showCheck ? (
+        <img src={Check} className={styles.icon} />
+      ) : (
+        <div className={styles.spacer} />
+      )}
+      <div className={styles.text}>{text}</div>
+    </div>
+  );
 };
 
 export default SidebarItem;
