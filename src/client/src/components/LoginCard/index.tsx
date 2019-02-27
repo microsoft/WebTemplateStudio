d3e917dcc2bbcbfaf23ee2417d00780cac63bbc9
Kelly Ng <t-keng@microsoft.com>
2019-02-27 15:32:31 -0800

    fix fonts within cards

--- src/client/src/components/LoginCard/index.tsx
+++ src/client/src/components/LoginCard/index.tsx
@@ -1,6 +1,8 @@
 import classnames from "classnames";
 import * as React from "react";
 
+import CardBody from "../CardBody";
+
 import buttonStyles from "../../css/buttonStyles.module.css";
 import styles from "./styles.module.css";
 
@@ -18,7 +20,9 @@ const LoginCard = ({ cardTitle, cardBody, svgUrl, handleClick }: IProps) => {
         <img className={styles.icon} src={svgUrl} />
         {cardTitle}
       </div>
-      <div className={styles.cardBody}>{cardBody}</div>
+      <div className={styles.cardBody}>
+        <CardBody body={cardBody} />
+      </div>
       <div className={styles.selectionContainer}>
         <div className={styles.details}>Details</div>
         <div className={styles.buttonContainer}>
