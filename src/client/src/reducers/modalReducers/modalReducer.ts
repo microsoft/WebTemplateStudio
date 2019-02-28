5ef4f38600c69148838f169c2b7e2354ca796424
Kelly <t-keng@microsoft.com>
2019-02-28 14:10:14 -0800

    cosmos modal to close on adding resource

--- src/client/src/reducers/modalReducers/modalReducer.ts
+++ src/client/src/reducers/modalReducers/modalReducer.ts
@@ -4,6 +4,7 @@ const openModal = (state = null, action: any) => {
     switch(action.type) {
         case Actions.OPEN_MODAL:
             return action.payload;
+        case Actions.SAVE_COSMOS_DB_RESOURCE_SETTINGS:
         case Actions.CLOSE_MODALS:
             return null;
         default:
