7834bc9368c0a70aaeff8aa01855cc2f0e7c7495
Kelly Ng <t-keng@microsoft.com>
2019-02-27 23:49:37 -0800

    add mock data for frameworks and made them unselectable

--- src/client/src/reducers/wizardContentReducers/backendFrameworkReducer.ts
+++ src/client/src/reducers/wizardContentReducers/backendFrameworkReducer.ts
@@ -9,7 +9,30 @@ import * as Actions from "../../actions/types";
 const backendFrameworkOptions = (state: any = [], action: any) => {
   switch (action.type) {
     case Actions.GET_BACKEND_FRAMEWORKS_SUCCESS:
-      return [...state, ...action.payload];
+      const newState = [...state];
+      for (const frameworkToAdd of action.payload) {
+        let found = false;
+        for (const framework of newState) {
+          if (framework.internalName === frameworkToAdd.internalName) {
+            found = true;
+          }
+        }
+        if (!found) {
+          newState.push(frameworkToAdd);
+        }
+      }
+      newState.sort(
+        (a: any, b: any): number => {
+          if (a.unselectable) {
+            return 1;
+          }
+          if (b.unselectable) {
+            return -1;
+          }
+          return 0;
+        }
+      );
+      return newState;
     default:
       return state;
   }
