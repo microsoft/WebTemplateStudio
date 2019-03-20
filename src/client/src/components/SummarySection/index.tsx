5b834d6056267545d6d9de02290b1f1a351495f6
Kelly <t-keng@microsoft.com>
2019-03-19 17:16:48 -0700

    allow function name changes from summary page

--- src/client/src/components/SummarySection/index.tsx
+++ src/client/src/components/SummarySection/index.tsx
@@ -16,31 +16,35 @@ interface IProps {
     isEditable?: boolean;
 }
 
+interface IStateProps {
+    functionApps: any;
+}
+
 interface IDispatchProps {
     updateFunctionNames: (functionApp: IFunctionApp) => any;
     removeAzureFunction: (functionIndex: number) => any;
 }
 
-type Props = IDispatchProps & IProps;
+type Props = IDispatchProps & IProps & IStateProps;
 
-const SummarySection = ({ selectionTitle, selectionRows, isEditable, removeAzureFunction, updateFunctionNames }: Props) => {
-    // const handleInputChange = (newTitle: string, idx: number) => {
-    //     const { functionNames } = functionApps.selection[0];
-    //     if (functionNames != null) {
-    //         functionNames[idx] = newTitle;
-    //         updateFunctionNames({
-    //             appIndex: 0,
-    //             functionNames
-    //         });
-    //     }
-    // };
+const SummarySection = ({ selectionTitle, selectionRows, isEditable, removeAzureFunction, updateFunctionNames, functionApps }: Props) => {
+    const handleAzureFuncNameChange = (newTitle: string, idx: number) => {
+        const { functionNames } = functionApps.selection[0];
+        if (functionNames != null) {
+            functionNames[idx] = newTitle;
+            updateFunctionNames({
+                appIndex: 0,
+                functionNames
+            });
+        }
+    };
     const handleRemoveFunction = (functionIndex: number) => {
         removeAzureFunction(functionIndex);
     }
-    const renderTile = (title: string, svgUrl?: string, company?: string, originalTitle?: string, isEditable?: boolean, withIndent?: boolean, handleCloseClick?: (idx: number) => any, idx?: number) => {
+    const renderTile = (title: string, svgUrl?: string, company?: string, originalTitle?: string, isEditable?: boolean, withIndent?: boolean, handleCloseClick?: (idx: number) => any, handleInputChange?: (newTitle: string, idx: number) => any, idx?: number) => {
         return (
-          <div className={styles.tileContainer} key={title + idx + originalTitle}>
-            <SummaryTile title={title} version="v1.0" svgUrl={svgUrl} company={company} originalTitle={originalTitle} isEditable={isEditable} withIndent={withIndent} handleCloseClick={handleCloseClick} idx={idx} />
+          <div className={styles.tileContainer} key={idx}>
+            <SummaryTile title={title} version="v1.0" svgUrl={svgUrl} company={company} originalTitle={originalTitle} isEditable={isEditable} withIndent={withIndent} handleCloseClick={handleCloseClick} handleInputChange={handleInputChange} idx={idx} />
           </div>
         )
     }
@@ -51,7 +55,7 @@ const SummarySection = ({ selectionTitle, selectionRows, isEditable, removeAzure
                     <React.Fragment>
                     {renderTile(selection.title, selection.svgUrl, selection.company, selection.originalTitle, isEditable)}
                     {selection.functionNames && selection.functionNames.map((functionName, idx: number) => (
-                        renderTile(functionName, undefined, undefined, undefined, true, true, handleRemoveFunction, idx + 1)
+                        renderTile(functionName, undefined, undefined, undefined, true, true, handleRemoveFunction, handleAzureFuncNameChange, idx + 1)
                     ))}
                     </React.Fragment>
                 ))}
@@ -59,9 +63,13 @@ const SummarySection = ({ selectionTitle, selectionRows, isEditable, removeAzure
     )
 }
 
+const mapStateToProps = (state: any): IStateProps => ({
+    functionApps: state.selection.services.azureFunctions
+})
+
 const mapDispatchToProps = (dispatch: any): IDispatchProps => ({
     updateFunctionNames: (functionApp: IFunctionApp) => { dispatch(AzureFunctionActions.updateAzureFunctionNamesAction(functionApp)) },
     removeAzureFunction: (functionIndex) => { dispatch(AzureFunctionActions.removeAzureFunctionAction(functionIndex)) }
 })
 
-export default connect(null, mapDispatchToProps)(SummarySection);
+export default connect(mapStateToProps, mapDispatchToProps)(SummarySection);
