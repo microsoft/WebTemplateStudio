f5b0169db876a84abd91393546ed3b0274813ccb
Kai Bailey <t-kabail@microsoft.com>
2019-03-21 09:44:00 -0700

    Added global margin for footer and updated package.json

--- mocks/materialui-multi-page/src/components/Footer/index.jsx
+++ mocks/materialui-multi-page/src/components/Footer/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./footer.module.css";
 
-export default function index() {
+export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className="container-fluid">
