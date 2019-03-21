1b0d9d0eedbd90e5f8b127eb40d236b775b0b00f
Kai Bailey <t-kabail@microsoft.com>
2019-03-21 10:29:31 -0700

    Convert the grid page to bootstrap css

--- mocks/materialui-multi-page/src/components/Grid/GridComponent.js
+++ mocks/materialui-multi-page/src/components/Grid/GridComponent.js
@@ -1,48 +1,11 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import { withStyles } from "@material-ui/core/styles";
 
-const styles = theme => ({
-  cardMedia: {
-    paddingTop: "56.25%" // 16:9
-  }
-});
-
-function GridComponent(props) {
-  const { classes } = props;
+export default function GridComponent(props) {
   return (
-    <Grid item sm={6} md={4}>
-      <Card>
-        <CardMedia
-          className={classes.cardMedia}
-          image={props.image}
-          title="Image title"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5">
-            {props.header}
-          </Typography>
-          <Typography>{props.description}</Typography>
-        </CardContent>
-        <Divider />
-        <CardActions>
-          <Button size="small" color="primary">
-            Action 1
-          </Button>
-          <Button size="small" color="primary">
-            Action 2
-          </Button>
-        </CardActions>
-      </Card>
-    </Grid>
+    <div className="col-4 pb-3">
+      <img src={props.image} alt="Default Grey Box" />
+      <h3>{props.header}</h3>
+      <p>{props.description}</p>
+    </div>
   );
 }
-
-export default withStyles(styles)(GridComponent);
--- mocks/materialui-multi-page/src/components/Grid/index.js
+++ mocks/materialui-multi-page/src/components/Grid/index.js
@@ -1,27 +1,10 @@
 import React, { Component } from "react";
 import GridComponent from "./GridComponent";
-import Grid from "@material-ui/core/Grid";
-import classNames from "classnames";
-import { withStyles } from "@material-ui/core/styles";
-import WarningMessage from "../WarningMessage";
-import defaultImage from "../../images/defaultImage.jpg";
+import GreyBox from "../../images/GreyBox.svg";
+import styles from "./grid.module.css";
+import classnames from "classnames";
 
-const styles = theme => ({
-  layout: {
-    marginRight: theme.spacing.unit * 4,
-    marginLeft: theme.spacing.unit * 4,
-    [theme.breakpoints.up("lg")]: {
-      width: 1100,
-      marginLeft: "auto",
-      marginRight: "auto"
-    }
-  },
-  cardGrid: {
-    padding: theme.spacing.unit * 6
-  }
-});
-
-class index extends Component {
+export default class Grid extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -60,27 +43,36 @@ class index extends Component {
   }
 
   render() {
-    const { classes } = this.props;
     return (
-      <div className={classNames(classes.layout, classes.cardGrid)}>
-        <Grid container spacing={40}>
-          {this.state.gridTextAssets.map(textAssets => (
-            <GridComponent
-              key={textAssets.id}
-              header={textAssets.header}
-              description={textAssets.description}
-              image={defaultImage}
-            />
-          ))}
-        </Grid>
-        <WarningMessage
-          open={this.state.WarningMessageOpen}
-          text={this.state.WarningMessageText}
-          onWarningClose={this.handleWarningClose}
-        />
-      </div>
+      <React.Fragment>
+        <div className={classnames("text-center", styles.header)}>
+          <h1>Project Acorn</h1>
+          <p>Full Stack boiler plate powered by Bootstrap 4.3</p>
+          <a
+            href="https://github.com/Microsoft/WebTemplateStudio"
+            className="btn btn-primary my-2"
+          >
+            Link to our Github
+          </a>
+        </div>
+
+        <div className="container">
+          <div className="row justify-content-center py-5">
+            <h1>Bootstrap Grid Template</h1>
+          </div>
+
+          <div className="row justify-content-around text-center pb-5">
+            {this.state.gridTextAssets.map(textAssets => (
+              <GridComponent
+                key={textAssets.id}
+                header={textAssets.header}
+                description={textAssets.description}
+                image={GreyBox}
+              />
+            ))}
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
-
-export default withStyles(styles)(index);
