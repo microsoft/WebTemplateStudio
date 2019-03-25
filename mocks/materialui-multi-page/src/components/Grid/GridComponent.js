c8991415363f2babf8bf23f1a8a746cfa6d87f9b
Kai Bailey <t-kabail@microsoft.com>
2019-03-25 09:51:34 -0700

    Added spacing between grid components

--- mocks/materialui-multi-page/src/components/Grid/GridComponent.js
+++ mocks/materialui-multi-page/src/components/Grid/GridComponent.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function GridComponent(props) {
   return (
-    <div className="col-4 pb-3">
-      <img src={props.image} alt="Default Grey Box" />
+    <div className="col-4 p-5">
+      <img src={props.image} alt="Default Grey Box" className="mb-3" />
       <h3>{props.header}</h3>
       <p>{props.description}</p>
     </div>
