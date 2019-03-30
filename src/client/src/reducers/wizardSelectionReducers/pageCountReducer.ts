88093fa21e3a77ce9f1b21c0667fc19172e24cb6
Kelly Ng <t-keng@microsoft.com>
2019-03-30 00:02:45 -0700

    autoselect blank page as an option in redux page count

--- src/client/src/reducers/wizardSelectionReducers/pageCountReducer.ts
+++ src/client/src/reducers/wizardSelectionReducers/pageCountReducer.ts
@@ -1,4 +1,5 @@
 import * as Actions from "../../actions/types";
+import { WIZARD_CONTENT_INTERNAL_NAMES } from "../../utils/constants";
 
 /* State Shape
 {
@@ -10,7 +11,9 @@ export interface IPageCount {
   [key: string]: number;
 }
 
-const initialState = {};
+const initialState = {
+  [WIZARD_CONTENT_INTERNAL_NAMES.BLANK_PAGE]: 1
+};
 
 const pageCountReducer = (state: IPageCount = initialState, action: any) => {
   switch (action.type) {
