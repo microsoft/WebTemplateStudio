d15c9402b365d8f33ef131927228838de198a0c1
Sahil Tara <t-satara@microsoft.com>
2019-04-01 15:22:49 -0700

    Fixed const bug

--- templates/Web/_composition/NodeJS/Page.Node.List.AddRoutes.NoCosmos/server/routes/index_postaction.js
+++ templates/Web/_composition/NodeJS/Page.Node.List.AddRoutes.NoCosmos/server/routes/index_postaction.js
@@ -1,7 +1,7 @@
-﻿const express = require("express");
-//{[{
+﻿//{[{
 const CONSTANTS = require("../constants");
 //}]}
+const express = require("express");
 //{[{
 const sampleData = require("../sampleData");
 //}]}
