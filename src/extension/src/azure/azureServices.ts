68982defec097f32cb99c5cc554de1a75da6c7e9
Sahil Tara <t-satara@microsoft.com>
2019-04-08 18:07:02 -0700

    Env file replacement pop up fix for Sql api (#349)

--- src/extension/src/azure/azureServices.ts
+++ src/extension/src/azure/azureServices.ts
@@ -1,4 +1,4 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import {
   AzureAuth,
   SubscriptionItem,
@@ -38,7 +38,6 @@ export abstract class AzureServices {
     return await AzureAuth.logout();
   }
   public static async getUserInfo() {
-
     this.subscriptionItemList = await AzureAuth.getSubscriptions();
 
     const subscriptionListToDisplay = this.subscriptionItemList.map(
@@ -233,7 +232,7 @@ export abstract class AzureServices {
       userCosmosDBSelection,
       genPath
     );
-  }  
+  }
   public static async promptUserForCosmosReplacement(
     pathToEnv: string,
     dbObject: DatabaseObject
