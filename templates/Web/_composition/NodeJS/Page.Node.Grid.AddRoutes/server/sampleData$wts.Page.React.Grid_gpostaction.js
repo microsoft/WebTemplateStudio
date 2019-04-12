1d85fa2fb11469544cfd55773b89754b34e332c1
Sahil Tara <t-satara@microsoft.com>
2019-04-11 20:37:27 -0700

    Changed heading to company 1...

--- templates/Web/_composition/NodeJS/Page.Node.Grid.AddRoutes/server/sampleData$wts.Page.React.Grid_gpostaction.js
+++ templates/Web/_composition/NodeJS/Page.Node.Grid.AddRoutes/server/sampleData$wts.Page.React.Grid_gpostaction.js
@@ -3,7 +3,7 @@ const shortLoremIpsum = `Lorem id sint aliqua tempor tempor sit. Ad dolor dolor 
 //}]}
 const sampleData = {};
 //{[{
-const header = "Heading";
+const header = "Company ";
 //}]}
 // This class holds sample data used by some generated pages to show how they can be used.
 // TODO Web Template Studio: Delete this file once your app is using real data.
@@ -11,47 +11,47 @@ const header = "Heading";
 sampleData.gridTextAssets = [
   {
     description: shortLoremIpsum,
-    header: header,
+    header: header + 1,
     id: 1
   },
   {
     description: shortLoremIpsum,
-    header: header,
+    header: header + 2,
     id: 2
   },
   {
     description: shortLoremIpsum,
-    header: header,
+    header: header + 3,
     id: 3
   },
   {
     description: shortLoremIpsum,
-    header: header,
+    header: header + 4,
     id: 4
   },
   {
     description: shortLoremIpsum,
-    header: header,
+    header: header + 5,
     id: 5
   },
   {
     description: shortLoremIpsum,
-    header: header,
+    header: header + 6,
     id: 6
   },
   {
     description: shortLoremIpsum,
-    header: header,
+    header: header + 7,
     id: 7
   },
   {
     description: shortLoremIpsum,
-    header: header,
+    header: header + 8,
     id: 8
   },
   {
     description: shortLoremIpsum,
-    header: header,
+    header: header + 9,
     id: 9
   }
 ];
