97495ed2b936b65e54230a2b03017fcd9e15c8bd
Kai Bailey <t-kabail@microsoft.com>
2019-04-22 08:47:38 -0700

    Added skip link to angular templates

--- templates/Web/Projects/AngularDefault/src/app/components/NavBar/navbar.component.ts
+++ templates/Web/Projects/AngularDefault/src/app/components/NavBar/navbar.component.ts
@@ -1,14 +1,12 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './navbar.component.html'
+  selector: "app-header",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
