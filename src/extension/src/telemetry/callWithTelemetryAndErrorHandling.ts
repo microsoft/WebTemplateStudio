979c1f55c5efeabcc60d298bf9dca985ee72d464
Trevor Ngo <t-trngo@microsoft.com>
2019-04-23 15:38:14 -0700

    Removed unused import

--- src/extension/src/telemetry/callWithTelemetryAndErrorHandling.ts
+++ src/extension/src/telemetry/callWithTelemetryAndErrorHandling.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { MessageItem, window, workspace } from 'vscode';
+import { MessageItem, window } from 'vscode';
 import { DialogResponses, DialogMessages } from '../constants';
 import { reportAnIssue } from './reportAnIssue';
 import { IParsedError, parseError } from './parseError';
