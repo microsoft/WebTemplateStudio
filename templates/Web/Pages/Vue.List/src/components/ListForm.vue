db55cf8886b87604483cacc8175fdfcf4e6c0e75
Sahil Tara <supertfriend@gmail.com>
2019-05-28 05:38:07 -0400

    Added vue list support

---
+++ templates/Web/Pages/Vue.List/src/components/ListForm.vue
@@ -0,0 +1,22 @@
+﻿<template>
+  <form class="input-group my-3" v-on:submit="$emit('onAddListItem')">
+    <input
+      v-bind:value="value"
+      v-on:input="$emit('input', $event.target.value)"
+      type="text"
+      class="form-control"
+      placeholder="Add text here..."
+      aria-label="Add text here..."
+    >
+    <button type="submit" class="btn btn-primary ml-2">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "ListForm",
+  props: {
+    value: String
+  }
+};
+</script>
