973f5d40abea4d87797d4445c99bd8adeab5d7f2
Nipun Jindal <t-nijind@microsoft.com>
2019-05-29 12:57:08 -0700

    complete list working; constants file; smapleData file;

---
+++ mocks/flask-react-mocks/server/sampleData.py
@@ -0,0 +1,105 @@
+import datetime
+
+shortLoremIpsum = "Lorem id sint aliqua tempor tempor sit. Ad dolor dolor ut nulla mollit dolore non eiusmod Lorem tempor nisi cillum."
+longLoremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet."
+
+sampleGridData = [
+  {
+    'shortDescription': shortLoremIpsum,
+    'longDescription': longLoremIpsum,
+    'title': 'Company A',
+    'status': 'Closed',
+    'shipTo': 'Francisco Pérez-Olaeta',
+    'orderTotal': 2490.0,
+    'orderDate': datetime.datetime.now(),
+    'id': 1
+  },
+  {
+    'shortDescription': shortLoremIpsum,
+    'longDescription': longLoremIpsum,
+    'title': 'Company B',
+    'status': 'Closed',
+    'shipTo': 'Soo Jung Lee',
+    'orderTotal': 1760.0,
+    'orderDate': datetime.datetime.now(),
+    'id': 2
+  },
+  {
+    'shortDescription': shortLoremIpsum,
+    'longDescription': longLoremIpsum,
+    'title': 'Company C',
+    'status': 'Shipped',
+    'shipTo': 'Run Liu',
+    'orderTotal': 665.0,
+    'orderDate': datetime.datetime.now(),
+    'id': 3
+  },
+  {
+    'shortDescription': shortLoremIpsum,
+    'longDescription': longLoremIpsum,
+    'title': "Company D",
+    'status': "Shipped",
+    'shipTo': "Soo Jung Lee",
+    'orderTotal': 560.0,
+    'orderDate': datetime.datetime.now(),
+    'id': 4
+  },
+  {
+    'shortDescription': shortLoremIpsum,
+    'longDescription': longLoremIpsum,
+    'title': "Company E",
+    'status': "New",
+    'shipTo': "John Rodman",
+    'orderTotal': 810.0,
+    'orderDate': datetime.datetime.now(),
+    'id': 5
+  },
+  {
+    'shortDescription': shortLoremIpsum,
+    'longDescription': longLoremIpsum,
+    'title': "Company F",
+    'status': "New",
+    'shipTo': "Elizabeth Andersen",
+    'orderTotal': 196.5,
+    'orderDate': datetime.datetime.now(),
+    'id': 6
+  },
+  {
+    'shortDescription': shortLoremIpsum,
+    'longDescription': longLoremIpsum,
+    'title': "Company G",
+    'status': "Closed",
+    'shipTo': "Peter Krschne",
+    'orderTotal': 270.0,
+    'orderDate': datetime.datetime.now(),
+    'id': 7
+  },
+  {
+    'shortDescription': shortLoremIpsum,
+    'longDescription': longLoremIpsum,
+    'title': "Company H",
+    'status': "Closed",
+    'shipTo': "Sven Mortensen",
+    'orderTotal': 736.0,
+    'orderDate': datetime.datetime.now(),
+    'id': 8
+  },
+  {
+    'shortDescription': shortLoremIpsum,
+    'longDescription': longLoremIpsum,
+    'title': "Company I",
+    'status': "Shipped",
+    'shipTo': "Anna Bedecs",
+    'orderTotal': 800.0,
+    'orderDate': datetime.datetime.now(),
+    'id': 9
+  }
+]
+
+sampleData = {
+    'listTextAssets': [
+        {'_id': 1, 'text': 'This is the list item 1'},
+        {'_id': 2, 'text': 'This is the list item 2'}
+    ],
+    'listId': 3
+}
--- mocks/flask-react-mocks/server/server.py
+++ mocks/flask-react-mocks/server/server.py
@@ -2,34 +2,39 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask import make_response
+from sampleData import *
+from constants import CONSTANTS
 import ast
+import datetime
 app = Flask(__name__)
 
-sampleData = [
-    {'_id': 1, 'text': 'This is the list item 1'},
-    {'_id': 2, 'text': 'This is the list item 2'}
-]
-
-@app.route('/')
-def sayHello():
-    return "Hello World"
-
 # List Endpoints
-@app.route('/api/list')
+@app.route(CONSTANTS['ENDPOINT']['LIST'])
 def getList():
     return jsonify(
-        sampleData
+        sampleData['listTextAssets']
     )
 
-@app.route('/api/list', methods = ['POST'])
+@app.route(CONSTANTS['ENDPOINT']['LIST'], methods = ['POST'])
 def addListItem():
     data = request.data.decode('utf-8')
     data = ast.literal_eval(data)
-    listItem = {'_id': 3, 'text': data['text']}
-    sampleData.append(listItem)
+    listItem = {'_id': sampleData['listId'], 'text': data['text']}
+    sampleData['listTextAssets'].append(listItem)
+    sampleData['listId'] += 1
     return jsonify(
         listItem
     )
 
+@app.route(CONSTANTS['ENDPOINT']['LIST'] + '/<int:id>', methods=['DELETE'])
+def deleteListItem(id):
+    listItemToRemove = [listItem for listItem in sampleData['listTextAssets'] if listItem['_id'] == id]
+    if(len(listItemToRemove) == 0):
+        return make_response(jsonify({'error': 'Could not find an item with given id'}))
+    sampleData['listTextAssets'] = [listItem for listItem in sampleData['listTextAssets'] if listItem['_id'] != id]
+    return jsonify(
+        {'_id': id, 'text': 'This comment was deleted'}
+    )
+
 if __name__ == '__main__':
-   app.run(debug=True, port=3001)
+   app.run(debug=True, port=CONSTANTS['PORT'])
