520adf4b9099334b1b67270c072a48c372221e67
Nipun Jindal <t-nijind@microsoft.com>
2019-05-29 12:10:00 -0700

    create a basic get route for initial list items

--- mocks/flask-react-mocks/server/server.py
+++ mocks/flask-react-mocks/server/server.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
+
+sampleData = [
+    {'_id': 1, 'text': 'This is the list item 1'},
+    {'_id': 2, 'text': 'This is the list item 2'}
+]
 
 @app.route('/')
 def sayHello():
     return "Hello World"
+
+# List Endpoints
+@app.route('/api/list')
+def getList():
+    return jsonify(
+        sampleData
+    )
 
 if __name__ == '__main__':
    app.run(debug=True, port=3001)
