e00ea41ded3b73ed531618b004c7e49944db0375
Nipun Jindal <t-nijind@microsoft.com>
2019-06-05 17:24:50 -0700

    remove import interface from master-detail.service

--- templates/Web/Pages/Angular.MasterDetail/src/app/app-shell/AngularMasterDetail/master-detail.service.ts
+++ templates/Web/Pages/Angular.MasterDetail/src/app/app-shell/AngularMasterDetail/master-detail.service.ts
@@ -2,7 +2,6 @@
 import { CONSTANTS } from 'src/constants';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { IMasterDetailText } from './MasterDetailPage/master-detail-page.component';
 
 @Injectable({
   providedIn: 'root'
