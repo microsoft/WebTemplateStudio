72eddb9a5afdcfcf140090e458b9197eea5e8ac7
Nipun Jindal <t-nijind@microsoft.com>
2019-06-10 15:12:45 -0700

    fix indentation

--- templates/Web/Projects/ReactDefault/src/index.jsx
+++ templates/Web/Projects/ReactDefault/src/index.jsx
@@ -8,7 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 ReactDOM.render(
     <BrowserRouter>
       <App />
-      </BrowserRouter>,
+    </BrowserRouter>,
   document.getElementById("root")
 );
 registerServiceWorker();
