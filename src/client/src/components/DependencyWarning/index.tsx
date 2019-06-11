4497a1c14d0ce81034cfef7d853f601ace1e7e71
Jason Doo <t-jedoo@microsoft.com>
2019-06-10 19:06:51 -0700

    c s s

--- src/client/src/components/DependencyWarning/index.tsx
+++ src/client/src/components/DependencyWarning/index.tsx
@@ -7,7 +7,7 @@ const DependencyWarning = () => {
   return (
     <div className={classnames(styles.warningDiv)}>
       <img className={styles.warningSvg} src={getSvg.getWarningSvg()} alt="" />
-      {/*TODO: react-intl */}
+      {/*TODO: react-intl, link to node/flask */}
       <div className={styles.body}>{"Node required. Click to install."}</div>
     </div>
   );
