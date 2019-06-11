c62d79e45888b5188b647312b4c6049384df4893
Tanya0609 <tanyi3025@gmail.com>
2019-06-11 13:52:23 -0700

    fix addHome

--- templates/Web/Projects/AngularDefault/src/app/app-routing.module.ts
+++ templates/Web/Projects/AngularDefault/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
 ];
 
@@ -14,24 +13,3 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-
-/*
-const routes: Routes = [
-  {
-    path: 'blank',
-    loadChildren: () => import('./app-shell/blank/blank.module').then(mod => mod.blankModule)
-  },
-  {
-    path: 'list',
-    loadChildren: () => import('./app-shell/list/list.module').then(mod => mod.listModule)
-  },
-  {
-    path: 'master_detail',
-    loadChildren: () => import('./app-shell/master_detail/master_detail.module').then(mod => mod.master_detailModule)
-  },
-  {
-    path: 'grid',
-    loadChildren: () => import('./app-shell/grid/grid.module').then(mod => mod.gridModule)
-  }
-];
-*/
