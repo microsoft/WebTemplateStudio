27ad07895af2f64d2497d69f9779c310e9744dd0
Jasmin Goh <t-ligoh@microsoft.com>
2019-06-11 10:53:31 -0700

    Set up stub for checking dependency

--- src/extension/src/constants.ts
+++ src/extension/src/constants.ts
@@ -216,7 +216,8 @@ export enum ExtensionCommand {
   CloseWizard = "close-wizard",
   GetPort = "get-port",
   ResetPages = "reset-pages",
-  GetPreviewStatus = "get-preview"
+  GetPreviewStatus = "get-preview",
+  CheckDependency = "check-dependency"
 }
 export enum ExtensionModule {
   Azure = "Azure",
