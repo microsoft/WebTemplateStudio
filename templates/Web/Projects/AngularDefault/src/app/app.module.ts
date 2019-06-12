00fcac3ff51bbe8a08c2230b3c5942288f10e387
Tanya0609 <tanyi3025@gmail.com>
2019-06-11 17:10:23 -0700

    blank as home page

--- templates/Web/Projects/AngularDefault/src/app/app.module.ts
+++ templates/Web/Projects/AngularDefault/src/app/app.module.ts
@@ -1,6 +1,5 @@
 ﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -17,7 +16,7 @@ import { FooterComponent } from './app-shell/footer/footer.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
