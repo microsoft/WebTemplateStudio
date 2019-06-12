275584a664379cfd328682876e7b7764b2e1ac0f
Tanya0609 <tanyi3025@gmail.com>
2019-06-12 09:48:03 -0700

    fix lint error

--- templates/Web/Pages/Angular.Grid/src/app/app-shell/AngularGrid/AngularGrid-routing.module.ts
+++ templates/Web/Pages/Angular.Grid/src/app/app-shell/AngularGrid/AngularGrid-routing.module.ts
@@ -17,9 +17,3 @@ const routes: Routes = [
 })
 export class AngularGridRoutingModule { }
 
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
