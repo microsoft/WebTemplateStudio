8fbe1d1b375ef5d12b54aacecec732e0b547f0f1
Tanya0609 <tanyi3025@gmail.com>
2019-06-13 09:41:18 -0700

    some naming convention, file structure, and routing (#711)
    * naming convention routing and lazy loading file structure

---
+++ templates/Web/Pages/Angular.Blank/src/app/app-shell/AngularBlank/AngularBlank-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { BlankComponent } from './blank.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BlankComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AngularBlankRoutingModule { }
+
