3fff306727fb64cf00f1dc2ddd4716d45051a2b4
Jasmin Goh <t-ligoh@microsoft.com>
2019-06-13 00:28:19 -0700

    Save work for now, will need to change payload structure

--- src/extension/src/utils/dependencyChecker.ts
+++ src/extension/src/utils/dependencyChecker.ts
@@ -29,29 +29,35 @@ export class DependencyChecker extends WizardServant {
     ]);
   }
 
-  async checkDependency(dependency: any): Promise<IPayloadResponse> {
+  async checkDependency(message: any): Promise<IPayloadResponse> {
     const child_process = require('child_process');  
-
-    var ls = child_process.spawn(dependency, ['--version']);
+    var name = message.payload.dependency;
+    var ls = child_process.spawn(name, ['--version']);
     ls.stdout.on('data', (data: any) => {
-      if (dependency === 'python') {
-        let outdated = data.indexOf('3.7.3')? false : true;
-        console.log('outdated? ' + outdated);  
-        // return payload 
-      } else {
-        // return payload 
+      if (name === 'python' && data.indexOf('3.7.3') === -1) { // python 3.7.3 is not installed
+        return {
+          payload: {
+            dependency: name,
+            installed: "Python 3.7.3 is required. Click to install."
+          }
+        };
       }
+      return {
+        payload: {
+          dependency: name,
+          installed: name + " is installed!"
+        }
+      };
     });
 
     ls.on('error', (err: any) => {
-      // return not installed payload
-    });
+      return {
+        payload: {
+          dependency: name,
+          notInstalled: name + " "
+        }
 
-    // stub
-    return {
-      payload: {
-        name: dependency,
-        installed: true
-    }};
+      }
+    });
   }
 }
