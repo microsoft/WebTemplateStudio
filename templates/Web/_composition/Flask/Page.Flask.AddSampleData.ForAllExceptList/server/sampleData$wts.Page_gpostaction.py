15c9664fb6720b1cb232f5262cfb4e9da9d59583
Nipun Jindal <t-nijind@microsoft.com>
2019-06-13 09:29:38 -0700

    PR feedback

--- templates/Web/_composition/Flask/Page.Flask.AddSampleData.ForAllExceptList/server/sampleData$wts.Page_gpostaction.py
+++ templates/Web/_composition/Flask/Page.Flask.AddSampleData.ForAllExceptList/server/sampleData$wts.Page_gpostaction.py
@@ -9,7 +9,7 @@ sampleData = {}
 longLoremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.'
 //}]}
 # This class holds sample data used by some generated pages to show how they can be used.
-# Todo Web Template Studio: Delete this file once your app is using real data.
+# TODO Web Template Studio: Delete this file once your app is using real data.
 //{[{
 sampleData['textAssets'] = [
   {
--- templates/Web/_composition/Flask/Page.Flask.AddSampleData.ForAllExceptList/server/sampleData.py
+++ templates/Web/_composition/Flask/Page.Flask.AddSampleData.ForAllExceptList/server/sampleData.py
@@ -1,5 +1,5 @@
 sampleData = {}
 
 # This class holds sample data used by some generated pages to show how they can be used.
-# Todo Web Template Studio: Delete this file once your app is using real data.
+# TODO Web Template Studio: Delete this file once your app is using real data.
 
