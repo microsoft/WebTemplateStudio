5c57844de009f4d09912de23c8b57f33a4f069a4
Jasmin Goh <t-ligoh@microsoft.com>
2019-06-13 11:49:17 -0700

    Use exec instead of spawn, and checks version

--- src/extension/src/utils/dependencyChecker.ts
+++ src/extension/src/utils/dependencyChecker.ts
@@ -1,10 +1,13 @@
-import * as vscode from "vscode";
 import { WizardServant, IPayloadResponse } from "../wizardServant";
-import {
-  ExtensionCommand,
-  DialogMessages,
-  DialogResponses
-} from "../constants";
+import { ExtensionCommand } from "../constants";
+const child_process = require('child_process');
+
+const NOT_INSTALLED = -1;
+const OUTDATED = 0;
+const INSTALLED = 1;
+
+const SUPPORTED_PYTHON_VERSION = '3.7.3';
+const SUPPORTED_NODE_VERSION_REGEX = /^v10\.[0-9]{1,2}\.[0-9]$/;
 
 export class DependencyChecker extends WizardServant {
   clientCommandMap: Map<
@@ -12,9 +15,6 @@ export class DependencyChecker extends WizardServant {
     (message: any) => Promise<IPayloadResponse>
   >;
 
-  /**
-   *
-   */
   constructor() {
     super();
     this.clientCommandMap = this.defineCommandMap();
@@ -30,34 +30,64 @@ export class DependencyChecker extends WizardServant {
   }
 
   async checkDependency(message: any): Promise<IPayloadResponse> {
-    const child_process = require('child_process');  
+    // keeping this here for now, not sure which is the better practice, spawn vs exec ??
+    // const child_process = require('child_process');  
+    // var name = message.payload.dependency;
+    // var ls = child_process.spawn(name, ['--version']);
+    // ls.stdout.on('data', (data: any) => {
+    //   if (name === 'python' && data.indexOf('3.7.3') === -1) { // python 3.7.3 is not installed
+    //     return {
+    //       payload: {
+    //         dependency: name,
+    //         info: 0
+    //       }
+    //     };
+    //   } else if (name === 'node' && data.indexOf('10.') === -1) { // node 10.x.x is not installed
+    //       return {
+    //         payload: {
+    //           dependency: name,
+    //           installed: 0
+    //         }
+    //       };
+    //   }});
+
+    // ls.on('error', (err: any) => {
+    //   return {
+    //     payload: {
+    //       dependency: name,
+    //       notInstalled: -1
+    //     }
+    //   };
+    // });
+
+    // return {
+    //   payload: {
+    //     dependency: name,
+    //     installed: 1
+    //   }
+    // };
+    
     var name = message.payload.dependency;
-    var ls = child_process.spawn(name, ['--version']);
-    ls.stdout.on('data', (data: any) => {
-      if (name === 'python' && data.indexOf('3.7.3') === -1) { // python 3.7.3 is not installed
-        return {
-          payload: {
-            dependency: name,
-            installed: "Python 3.7.3 is required. Click to install."
-          }
-        };
+    var state;
+    child_process.exec(name + " --version", (err: any, stdout: any, stderr: any) => {
+      if ((err !== null || stderr.length > 0) && stdout.length === 0) {
+        state = NOT_INSTALLED;
       }
-      return {
-        payload: {
-          dependency: name,
-          installed: name + " is installed!"
+      else {
+        if ((name === 'python' && stdout.indexOf(SUPPORTED_PYTHON_VERSION) !== -1) ||
+            (name === 'node'   && !SUPPORTED_NODE_VERSION_REGEX.test(stdout))) {
+            state = OUTDATED;
         }
-      };
-    });
-
-    ls.on('error', (err: any) => {
-      return {
-        payload: {
-          dependency: name,
-          notInstalled: name + " "
+        else {
+          state = INSTALLED;
         }
-
       }
     });
+    return {
+      payload: {
+        dependency: name,
+        state: state
+      }
+    };
   }
 }
