20cbcc03a01ce893f4b28f619eac154f81ce64f7
Nipun Jindal <t-nijind@microsoft.com>
2019-06-13 23:23:49 -0700

    add masterDetail Routes

--- templates/Web/_composition/Flask/Page.Flask.MasterDetail.AddRoutes/server/constants_postaction.py
+++ templates/Web/_composition/Flask/Page.Flask.MasterDetail.AddRoutes/server/constants_postaction.py
@@ -7,7 +7,7 @@ CONSTANTS['ENDPOINT'] = {}
 //}]}
 
 //{[{
-CONSTANTS['ENDPOINT']['GRID'] = '/api/grid'
+CONSTANTS['ENDPOINT']['MASTERDETAIL'] = '/api/masterdetail'
 //}]}
 
 CONSTANTS['PORT'] = os.environ.get('PORT', 3001)
