05985a7a3cbd4bce8883631c0653a4a5c0f2bfb1
Jasmin Goh <t-ligoh@microsoft.com>
2019-06-17 12:31:04 -0700

    Async code for dependency checker

--- src/extension/src/utils/dependencyChecker.ts
+++ src/extension/src/utils/dependencyChecker.ts
@@ -1,13 +1,14 @@
 import { WizardServant, IPayloadResponse } from "../wizardServant";
 import { ExtensionCommand } from "../constants";
-const child_process = require("child_process");
+// const child_process = require("child_process");
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
-const NOT_INSTALLED = -1;
-const OUTDATED = 0;
+const NOT_INSTALLED = 0;
 const INSTALLED = 1;
 
-const SUPPORTED_PYTHON_VERSION = "3.7.3";
-const SUPPORTED_NODE_VERSION_REGEX = /^v10\.[0-9]{1,2}\.[0-9]$/;
+// const SUPPORTED_PYTHON_VERSION = "3.7.3";
+// const SUPPORTED_NODE_VERSION_REGEX = /^v10\.[0-9]{1,2}\.[0-9]$/;
 
 export class DependencyChecker extends WizardServant {
   clientCommandMap: Map<
@@ -27,28 +28,27 @@ export class DependencyChecker extends WizardServant {
     return new Map([[ExtensionCommand.CheckDependency, this.checkDependency]]);
   }
 
+  // private runVersionCommand(name: any) {
+  //     return Promise
+    
+  // }
+
   async checkDependency(message: any): Promise<IPayloadResponse> {
     var name = message.payload.dependency;
     var state;
-    var stdout;
+
+
     try {
-      stdout = child_process
-        .execSync(name + " --version")
-        .toString()
-        .trim();
-      if (
-        (name === "python" &&
-          stdout.indexOf(SUPPORTED_PYTHON_VERSION) !== -1) ||
-        (name === "node" && SUPPORTED_NODE_VERSION_REGEX.test(stdout))
-      ) {
+      const { stdout, stderr } = await exec(name + ' --version');
+      if (stdout.length > 0) {
         state = INSTALLED;
-      } else {
-        state = OUTDATED;
+      } else if (stderr.length > 0) {
+        state = NOT_INSTALLED;
       }
-    } catch (error) {
+    } catch (err) {
       state = NOT_INSTALLED;
     }
-
+    console.log("This is state: " + state);
     return {
       payload: {
         dependency: name,
