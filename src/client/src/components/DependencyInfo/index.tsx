e4d601018c9cd741b2052c662865404c09f31c16
Jason Doo <t-jedoo@microsoft.com>
2019-06-18 12:11:49 -0700

    commit before doing intl

--- src/client/src/components/DependencyInfo/index.tsx
+++ src/client/src/components/DependencyInfo/index.tsx
@@ -3,59 +3,67 @@ import styles from "./styles.module.css";
 import * as getSvg from "../../utils/getSvgUrl";
 import classnames from "classnames";
 
+interface IDependencyInfoProps {
+  frameworkName: string;
+  installed: boolean;
+}
+
+type Props = IDependencyInfoProps;
+
 /*
  * Props:
- * - frameworkName: "node" | "flask"
+ * - frameworkName: "NodeJS" | "flask"
  * - installed: boolean, true if correct version installed, otherwise false
  */
-const DependencyInfo: any = (props: any) => {
-  let frameworkName = props.frameworkName;
-  let installed = props.installed;
+class DependencyInfo extends React.Component<Props> {
+  public render() {
+    let { frameworkName, installed } = this.props;
 
-  let dependencyMessage: string;
+    let dependencyMessage: string;
+    if (installed) {
+      dependencyMessage = " detected!";
+    } else {
+      dependencyMessage = " not detected. Click to install.";
+    }
 
-  if (installed) {
-    dependencyMessage = " detected!";
-  } else {
-    dependencyMessage = " not detected. Click to install.";
-  }
-  let downloadLink: string = "";
-  let icon: any = getSvg.getGreenCheckSvg();
+    let downloadLink: string = "";
+    let icon: any = getSvg.getGreenCheckSvg();
 
-  if (frameworkName === "NodeJS") {
-    downloadLink = "https://nodejs.org/en/download/";
-  } else if (frameworkName === "Flask") {
-    downloadLink = "https://www.python.org/downloads/";
-  }
+    if (frameworkName === "NodeJS") {
+      downloadLink = "https://nodejs.org/en/download/";
+    } else if (frameworkName === "Flask") {
+      downloadLink = "https://www.python.org/downloads/";
+    }
 
-  if (installed) {
-    icon = getSvg.getGreenCheckSvg();
-  } else {
-    icon = getSvg.getWarningSvg();
-  }
+    if (installed) {
+      icon = getSvg.getGreenCheckSvg();
+    } else {
+      icon = getSvg.getWarningSvg();
+    }
 
-  return (
-    <a
-      target={"_blank"}
-      href={downloadLink}
-      className={classnames(styles.dependencyContainer, {
-        [styles.disabled]: installed,
-        [styles.borderGreen]: installed,
-        [styles.borderYellow]: !installed
-      })}
-    >
-      <img className={styles.icon} src={icon} alt="" />
-      {/*TODO: react-intl */}
-      <div
-        className={classnames(styles.body, {
-          [styles.bodyGreen]: installed,
-          [styles.bodyYellow]: !installed
+    return (
+      <a
+        target={"_blank"}
+        href={downloadLink}
+        className={classnames(styles.dependencyContainer, {
+          [styles.disabled]: installed,
+          [styles.borderGreen]: installed,
+          [styles.borderYellow]: !installed
         })}
       >
-        {`${frameworkName} ${dependencyMessage}`}
-      </div>
-    </a>
-  );
-};
+        <img className={styles.icon} src={icon} alt="" />
+        {/*TODO: react-intl */}
+        <div
+          className={classnames(styles.body, {
+            [styles.bodyGreen]: installed,
+            [styles.bodyYellow]: !installed
+          })}
+        >
+          {`${frameworkName} ${dependencyMessage}`}
+        </div>
+      </a>
+    );
+  }
+}
 
 export default DependencyInfo;
