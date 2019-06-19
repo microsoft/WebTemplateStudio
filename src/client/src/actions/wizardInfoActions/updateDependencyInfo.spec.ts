c694cd1b1923cb9926ad461809aae7c44e7bb1bd
Jason Doo <t-jedoo@microsoft.com>
2019-06-19 10:49:46 -0700

    action test change

--- src/client/src/actions/wizardInfoActions/updateDependencyInfo.spec.ts
+++ src/client/src/actions/wizardInfoActions/updateDependencyInfo.spec.ts
@@ -3,9 +3,10 @@ import { WIZARD_INFO_TYPEKEYS } from "./typeKeys";
 
 describe("updateDependencyInfo actions", () => {
   it("should create an action", () => {
-    expect(updateDependencyInfoAction({ a: 1 })).toEqual({
+    const dependencyInfo = { dependency: "node" };
+    expect(updateDependencyInfoAction(dependencyInfo)).toEqual({
       type: WIZARD_INFO_TYPEKEYS.UPDATE_DEPENDENCY_INFO,
-      payload: { a: 1 }
+      payload: dependencyInfo
     });
   });
 });
