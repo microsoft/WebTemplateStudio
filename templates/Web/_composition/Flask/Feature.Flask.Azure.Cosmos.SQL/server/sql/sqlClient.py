51b3cee411a0a7ba115054bc5b2a9540c96bd46c
Nipun Jindal <t-nijind@microsoft.com>
2019-06-20 10:40:04 -0700

    stylistic change

--- templates/Web/_composition/Flask/Feature.Flask.Azure.Cosmos.SQL/server/sql/sqlClient.py
+++ templates/Web/_composition/Flask/Feature.Flask.Azure.Cosmos.SQL/server/sql/sqlClient.py
@@ -6,11 +6,21 @@ from .settings import *
 class SQLObj():
     def __init__(self):
         self.client = cosmos_client.CosmosClient(
-            url_connection=connection_key, auth={'masterKey': master_key})
+            url_connection=connection_key,
+            auth={'masterKey': master_key}
+        )
         self.db = self.client.CreateDatabase(
-            {'id': CONSTANTS['COSMOS']['DATABASE']})
-        self.container = self.client.CreateContainer(self.db['_self'], {
-                                                     'id': CONSTANTS['COSMOS']['CONTAINER']}, {'offerThroughput': 400})
+            {'id': CONSTANTS['COSMOS']['DATABASE']}
+        )
+        self.container = self.client.CreateContainer(
+            self.db['_self'],
+            {
+                'id': CONSTANTS['COSMOS']['CONTAINER']
+            }, 
+            {
+                'offerThroughput': 400
+            }
+        )
 
     def getClient(self):
         return self.client
