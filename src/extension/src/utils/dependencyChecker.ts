72286d2ae79850336a960ac282a14194dc7f807f
Jason Doo <t-jedoo@microsoft.com>
2019-06-24 13:53:08 -0700

    if else into 1 line

--- src/extension/src/utils/dependencyChecker.ts
+++ src/extension/src/utils/dependencyChecker.ts
@@ -1,11 +1,11 @@
 import { WizardServant, IPayloadResponse } from "../wizardServant";
 import { ExtensionCommand, CONSTANTS } from "../constants";
-const os = require('os');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const os = require("os");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
-const PYTHON3_REGEX = RegExp('^Python 3\\.[5-9]\\.[0-9]');       // minimum Python version required is 3.5.x
-const NODE_REGEX = RegExp('v10\\.(1[5-9]|[2-9][0-9])\\.[0-9]');  // minimum Node version required is 10.15.x
+const PYTHON3_REGEX = RegExp("^Python 3\\.[5-9]\\.[0-9]"); // minimum Python version required is 3.5.x
+const NODE_REGEX = RegExp("v10\\.(1[5-9]|[2-9][0-9])\\.[0-9]"); // minimum Node version required is 10.15.x
 
 export class DependencyChecker extends WizardServant {
   clientCommandMap: Map<
@@ -28,12 +28,8 @@ export class DependencyChecker extends WizardServant {
   private async runPythonVersionCommand(command: string) {
     let installed: boolean;
     try {
-      const { stdout } = await exec(command + ' --version');
-      if (PYTHON3_REGEX.test(stdout)) {
-        installed = true;
-      } else {
-        installed = false;
-      }
+      const { stdout } = await exec(command + " --version");
+      installed = PYTHON3_REGEX.test(stdout);
     } catch (err) {
       installed = false;
     }
@@ -44,27 +40,32 @@ export class DependencyChecker extends WizardServant {
     let name: string = message.payload.dependency;
     let state: boolean = false;
     if (name === CONSTANTS.DEPENDENCY_CHECKER.NODE) {
-      try { 
-        const { stdout } = await exec(CONSTANTS.DEPENDENCY_CHECKER.NODE + ' --version');
-        if (NODE_REGEX.test(stdout)) { 
-          state = true;
-        } else {
-          state = false;
-        }
+      try {
+        const { stdout } = await exec(
+          CONSTANTS.DEPENDENCY_CHECKER.NODE + " --version"
+        );
+        state = NODE_REGEX.test(stdout);
       } catch (err) {
         state = false;
       }
-    }
-
-    else if (name === CONSTANTS.DEPENDENCY_CHECKER.PYTHON) {
+    } else if (name === CONSTANTS.DEPENDENCY_CHECKER.PYTHON) {
       let userOS: string = os.platform();
-      let userOnWin: boolean = userOS.indexOf('win') === 0;
+      let userOnWin: boolean = userOS.indexOf("win") === 0;
 
-      if (await this.runPythonVersionCommand(CONSTANTS.DEPENDENCY_CHECKER.PYTHON3)) {
+      if (
+        await this.runPythonVersionCommand(CONSTANTS.DEPENDENCY_CHECKER.PYTHON3)
+      ) {
         state = true;
-      } else if (await this.runPythonVersionCommand(CONSTANTS.DEPENDENCY_CHECKER.PYTHON)) {
+      } else if (
+        await this.runPythonVersionCommand(CONSTANTS.DEPENDENCY_CHECKER.PYTHON)
+      ) {
         state = true;
-      } else if (userOnWin && await this.runPythonVersionCommand(CONSTANTS.DEPENDENCY_CHECKER.PYTHON_LAUNCHER)) {
+      } else if (
+        userOnWin &&
+        (await this.runPythonVersionCommand(
+          CONSTANTS.DEPENDENCY_CHECKER.PYTHON_LAUNCHER
+        ))
+      ) {
         state = true;
       } else {
         state = false;
