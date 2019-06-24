f834ebc742df9b8dd6ed9d729cda4e90cf7d30ab
Annie Liang <t-anlian@microsoft.com>
2019-06-24 16:04:31 -0700

    moved functions outside of attemptSync

--- src/extension/src/scripts/generate-test.ts
+++ src/extension/src/scripts/generate-test.ts
@@ -7,6 +7,7 @@ let frontends: string[] = [];
 let pagesObj: any[] = [];
 const projType = "FullStackWebApp";
 let syncAttemptNum = 0;
+let prevPromise: Promise<any> = Promise.resolve(null);
 
 const delay = (time: number) => {
   return new Promise(function(resolve) {
@@ -14,96 +15,103 @@ const delay = (time: number) => {
   });
 };
 
+let getPagesObj = (
+  instance: CoreTemplateStudio,
+  frontend: any,
+  backend: any
+) => {
+  return instance
+    .getPages(projType, frontend, backend)
+    .then(pages => {
+      pagesObj = [];
+      pages.forEach((page: { name: any; templateId: any }) => {
+        pagesObj.push({
+          name: page.name,
+          identity: page.templateId
+        });
+      });
+      return pagesObj;
+    })
+    .catch((error: Error) => {
+      console.log(error.toString());
+    });
+};
+
+let generateProj = (
+  instance: CoreTemplateStudio,
+  backend: string,
+  frontend: string
+) => {
+  return getPagesObj(instance, frontend, backend).then(pagesObj => {
+    return instance.generate({
+      port: instance.getPort(),
+      payload: {
+        backendFramework: backend,
+        frontendFramework: frontend,
+        pages: pagesObj,
+        path: "../../../../../src/extension/src/template_test",
+        projectName: backend + "-" + frontend,
+        projectType: projType,
+        services: []
+      },
+      liveMessageHandler: value => {
+        value;
+      }
+    });
+  });
+};
+
 let attemptSync: any = (
   instanceObj: CoreTemplateStudio,
   syncAttemptNum: number
 ) => {
-  if (syncAttemptNum < CONSTANTS.API.MAX_SYNC_REQUEST_ATTEMPTS) {
-    return instanceObj
-      .sync({
-        port: instance.getPort(),
-        payload: { path: CONSTANTS.API.DEVELOPMENT_PATH_TO_TEMPLATES },
-        liveMessageHandler: value => {
-          value;
-        }
-      })
-      .then(() => {
-        instance
-          .getFrameworks(projType)
-          .then(frameworks => {
-            frameworks.forEach(
-              (obj: { tags: { type: string }; name: string }) => {
-                if (obj.tags.type == "frontend") {
-                  frontends.push(obj.name);
-                } else if (obj.tags.type == "backend") {
-                  backends.push(obj.name);
-                }
-              }
-            );
-
-            let getPagesObj = (frontend: any, backend: any) => {
-              return instance
-                .getPages(projType, frontend, backend)
-                .then(pages => {
-                  pagesObj = [];
-                  pages.forEach((page: { name: any; templateId: any }) => {
-                    pagesObj.push({
-                      name: page.name,
-                      identity: page.templateId
-                    });
-                  });
-                  return pagesObj;
-                })
-                .catch((error: Error) => {
-                  console.log(error.toString());
-                });
-            };
-
-            let generateProj = (backend: string, frontend: string) => {
-              return getPagesObj(frontend, backend).then(pagesObj => {
-                return instance.generate({
-                  port: instance.getPort(),
-                  payload: {
-                    backendFramework: backend,
-                    frontendFramework: frontend,
-                    pages: pagesObj,
-                    path: "../../../../../src/extension/src/template_test",
-                    projectName: backend + "-" + frontend,
-                    projectType: projType,
-                    services: []
-                  },
-                  liveMessageHandler: value => {
-                    value;
-                  }
-                });
-              });
-            };
-
-            let prevPromise: Promise<any> = Promise.resolve(null);
-            frontends.forEach(frontendFrameWork => {
-              backends.forEach(backendFramework => {
-                prevPromise = prevPromise.then(() =>
-                  generateProj(backendFramework, frontendFrameWork)
-                );
-              });
-            });
-
-            prevPromise.then(() => {
-              console.log("done");
-            });
-          })
-          .catch((error: Error) => {
-            throw Error(error.toString());
-          });
-      })
-      .catch(() => {
-        syncAttemptNum++;
-        return delay(3000).then(() => attemptSync(instance, syncAttemptNum));
-      });
-  } else {
+  if (syncAttemptNum >= CONSTANTS.API.MAX_SYNC_REQUEST_ATTEMPTS) {
     CoreTemplateStudio.DestroyInstance();
     throw new Error("too many failed sync requests");
   }
+
+  return instanceObj
+    .sync({
+      port: instance.getPort(),
+      payload: { path: CONSTANTS.API.DEVELOPMENT_PATH_TO_TEMPLATES },
+      liveMessageHandler: value => {
+        value;
+      }
+    })
+    .then(() => {
+      instance
+        .getFrameworks(projType)
+        .then(frameworks => {
+          frameworks.forEach(
+            (obj: { tags: { type: string }; name: string }) => {
+              if (obj.tags.type == "frontend") {
+                frontends.push(obj.name);
+              } else if (obj.tags.type == "backend") {
+                backends.push(obj.name);
+              }
+            }
+          );
+
+          frontends.forEach(frontendFrameWork => {
+            backends.forEach(backendFramework => {
+              prevPromise = prevPromise.then(() =>
+                generateProj(instance, backendFramework, frontendFrameWork)
+              );
+            });
+          });
+
+          prevPromise.then(() => {
+            console.log("done");
+          });
+        })
+        .catch((error: Error) => {
+          throw Error(error.toString());
+        });
+    })
+    .catch(() => {
+      syncAttemptNum++;
+      return delay(3000).then(() => attemptSync(instance, syncAttemptNum));
+    });
 };
 
 CoreTemplateStudio.GetInstance(undefined)
