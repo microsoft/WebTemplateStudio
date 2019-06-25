15ab395d0ff8b9c82c04e002539e3557f1c0dce5
Nipun Jindal <35667299+njindal239@users.noreply.github.com>
2019-06-25 13:29:52 -0700

    Appropriate README.md should be generated with a Flask Project (#789)
    * add readme files
    
    * readme update
    
    * add readme files
    
    * readme update
    
    * PR Feedback: small fix in readme

--- templates/Web/Pages/Angular.List/src/app/app-shell/Param_SourceName_Kebab/list.service.spec.ts
+++ templates/Web/Pages/Angular.List/src/app/app-shell/Param_SourceName_Kebab/list.service.spec.ts
@@ -1,4 +1,4 @@
-import { async, TestBed } from '@angular/core/testing';
+﻿import { async, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ListService } from './list.service';
