6e38b67d64e72c02a9f71619e36a333686abbb64
Nipun Jindal <t-nijind@microsoft.com>
2019-06-25 13:51:02 -0700

    fix typo

--- templates/Web/_composition/Flask/Page.Flask.MasterDetail.AddRoutes/server/server_postaction.py
+++ templates/Web/_composition/Flask/Page.Flask.MasterDetail.AddRoutes/server/server_postaction.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 //{[{
 # MasterDetail Page Endpoint
 @app.route(CONSTANTS['ENDPOINT']['MASTER_DETAIL'])
-def get_msater_detail():
+def get_master_detail():
     return jsonify(
         sample_data['text_assets']
     )
