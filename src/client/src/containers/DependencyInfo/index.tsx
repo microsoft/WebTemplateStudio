2136ebc60d1a94d31ba902011e35902937b0a52b
Jason Doo <t-jedoo@microsoft.com>
2019-07-02 22:33:46 -0700

    change div to have role=button

--- src/client/src/containers/DependencyInfo/index.tsx
+++ src/client/src/containers/DependencyInfo/index.tsx
@@ -108,6 +108,7 @@ class DependencyInfo extends React.Component<Props> {
 
     return (
       <div
+        role="button"
         onClick={() => openPrivacyModal(dependency)}
         className={classnames(styles.dependencyContainer, {
           [styles.disabled]: installed,
