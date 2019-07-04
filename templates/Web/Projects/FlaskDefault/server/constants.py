d3847805a98fefbaa8c1a1842e71551a81fc141d
Nipun Jindal <t-nijind@microsoft.com>
2019-07-04 15:35:00 -0700

    handle template errors

--- templates/Web/Projects/FlaskDefault/server/constants.py
+++ templates/Web/Projects/FlaskDefault/server/constants.py
@@ -4,5 +4,5 @@ CONSTANTS = {
     'PORT': os.environ.get('PORT', 3001),
     'HTTP_STATUS': {
         '404_NOT_FOUND': 404,
-    }
+    },
 }
--- templates/Web/_composition/Flask/Page.Flask.List.AddMerging/server/constants_postaction.py
+++ templates/Web/_composition/Flask/Page.Flask.List.AddMerging/server/constants_postaction.py
@@ -2,16 +2,16 @@ import os
 
 CONSTANTS = {
     'PORT': os.environ.get('PORT', 3001),
-    'ENDPOINT': {
-        //{[{
-        'LIST': '/api/list',
-        //}]}
-    },
     'HTTP_STATUS': {
         '404_NOT_FOUND': 404,
         //{[{
         '201_CREATED': 201,
         '500_INTERNAL_SERVER_ERROR': 500
+        //}]}
+    },
+    'ENDPOINT': {
+        //{[{
+        'LIST': '/api/list',
         //}]}
     }
 }
