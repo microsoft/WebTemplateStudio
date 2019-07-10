e7e93d47cd5a625014a4b2bbd138767ca5fcef40
Jasmin Goh <t-ligoh@microsoft.com>
2019-07-09 17:07:40 -0700

    Removed getFeature method since it is not used

--- src/client/src/services/EngineAPIService.tsx
+++ src/client/src/services/EngineAPIService.tsx
@@ -12,30 +12,6 @@ export default class EngineAPIService {
     }
   }
 
-  public async getFeatures(
-    projectType: string,
-    frontendFramework: string,
-    backendFramework: string
-  ): Promise<any> {
-    const url = new URL(constants.API.Endpoints.Feature, this.API);
-    url.searchParams.append(constants.API.QueryParams.ProjectType, projectType);
-    url.searchParams.append(
-      constants.API.QueryParams.FrontendFramework,
-      frontendFramework
-    );
-    url.searchParams.append(
-      constants.API.QueryParams.BackendFramework,
-      backendFramework
-    );
-    return await fetch(url.href, { method: constants.API.Methods.GET })
-      .then((response: Response) => {
-        return response.json();
-      })
-      .catch((error: Error) => {
-        throw Error(error.toString());
-      });
-  }
-
   public async getPages(
     projectType: string,
     frontendFramework: string,
