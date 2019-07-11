24b4bbd8b0b408cde98af41bafd665c28f63a0de
Jason Doo <t-jedoo@microsoft.com>
2019-07-11 15:56:56 -0700

    always blue

--- src/client/src/containers/AzureSubscriptions/index.tsx
+++ src/client/src/containers/AzureSubscriptions/index.tsx
@@ -129,7 +129,7 @@ class AzureSubscriptions extends React.Component<Props, IState> {
               )}
               disabled={!isLoggedIn}
               handleDetailsClick={setDetailPage}
-              useNormalButtons={true}
+              useNormalButtons={false}
             />
           </div>
         ))}
