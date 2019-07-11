5ea39fb95f5eeb66cb778a9f5676d2452383a724
Jasmin Goh <t-ligoh@microsoft.com>
2019-07-11 13:56:10 -0700

    Merged getProjectTypes and getProjectTypesSuccess

--- src/client/src/actions/wizardContentActions/getProjectTypes.ts
+++ src/client/src/actions/wizardContentActions/getProjectTypes.ts
@@ -1,11 +1,16 @@
+import { Dispatch } from "react";
 import { IMetadata } from "../../types/metadata";
 import { IOption } from "../../types/option";
-import { getProjectTypesSuccess } from "./getProjectTypesSuccess";
 import getSvgUrl from "../../utils/getSvgUrl";
-import { Dispatch } from "react";
+import { WIZARD_CONTENT_TYPEKEYS } from "./typeKeys";
+
+export interface IProjectTypesActionType {
+  type: WIZARD_CONTENT_TYPEKEYS.GET_PROJECT_TYPES_SUCCESS;
+  payload: IOption[];
+}
 
 export const getProjectTypesAction = () => {
-  // TODO: 
+  // TODO:
   return getProjectTypesSuccess(
     getOptionalFromMetadata(getMetadataFromJson([]))
   );
