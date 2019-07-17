c8ebd3580c0758236550d26fd64818777a6a532f
Sahil Tara <supertfriend@gmail.com>
2019-07-17 12:14:13 -0400

    Better reset wizard handling + Output path error message fix (#893)
    * Moved reset action handling to root reducer
    
    * Fixed the reset wizard output path bug
    
    * Added clarifying comment

--- src/client/src/reducers/wizardSelectionReducers/selectPagesReducer.ts
+++ src/client/src/reducers/wizardSelectionReducers/selectPagesReducer.ts
@@ -1,8 +1,6 @@
 import WizardSelectionActionType from "../../actions/wizardSelectionActions/wizardSelectionActionType";
 import { ISelected } from "../../types/selected";
 import { WIZARD_SELECTION_TYPEKEYS } from "../../actions/wizardSelectionActions/typeKeys";
-import { WIZARD_INFO_TYPEKEYS } from "../../actions/wizardInfoActions/typeKeys";
-import WizardInfoType from "../../actions/wizardInfoActions/wizardInfoActionType";
 
 /* State Shape
 {
@@ -12,14 +10,13 @@ import WizardInfoType from "../../actions/wizardInfoActions/wizardInfoActionType
 
 const pagesReducer = (
   state: ISelected[] = [],
-  action: WizardSelectionActionType | WizardInfoType
+  action: WizardSelectionActionType
 ) => {
   switch (action.type) {
     case WIZARD_SELECTION_TYPEKEYS.SELECT_PAGES:
       const newPages: ISelected[] = [...action.payload];
       return newPages;
     case WIZARD_SELECTION_TYPEKEYS.RESET_PAGES:
-    case WIZARD_INFO_TYPEKEYS.RESET_WIZARD:
       return [];
     default:
       return state;
--- src/client/src/reducers/wizardSelectionReducers/updateOutputPath.ts
+++ src/client/src/reducers/wizardSelectionReducers/updateOutputPath.ts
@@ -1,8 +1,6 @@
 import { FormattedMessage } from "react-intl";
 import { WIZARD_SELECTION_TYPEKEYS } from "../../actions/wizardSelectionActions/typeKeys";
 import WizardSelectionActionType from "../../actions/wizardSelectionActions/wizardSelectionActionType";
-import { WIZARD_INFO_TYPEKEYS } from "../../actions/wizardInfoActions/typeKeys";
-import WizardInfoType from "../../actions/wizardInfoActions/wizardInfoActionType";
 
 /* State Shape
 {
@@ -31,7 +29,7 @@ const initialState = {
 
 const outputPathReducer = (
   state: IOutputPath = initialState,
-  action: WizardSelectionActionType | WizardInfoType
+  action: WizardSelectionActionType
 ) => {
   switch (action.type) {
     case WIZARD_SELECTION_TYPEKEYS.UPDATE_OUTPUT_PATH:
@@ -46,8 +44,6 @@ const outputPathReducer = (
           ...action.payload
         }
       };
-    case WIZARD_INFO_TYPEKEYS.RESET_WIZARD:
-      return initialState;
     default:
       return state;
   }
