71811290579fd18e416f2948ce0e9460a59bf251
Piotr Markowski <pimarkow@microsoft.com>
2019-07-23 09:28:31 -0700

    Revert changes from another branch

--- src/extension/src/signalr-api-module/generateCommand.ts
+++ src/extension/src/signalr-api-module/generateCommand.ts
@@ -5,21 +5,14 @@ import { IEngineGenerationPayloadType } from "../types/engineGenerationPayloadTy
 
 export class GenerateCommand extends CoreTemplateStudioApiCommand {
   async performCommandAction(connection: signalR.HubConnection): Promise<any> {
-    const payload = <IGenerationPayloadType>this.commandPayload.payload;
-    const body = this.makeEngineGenerationPayload(payload);
+    let body = this.makeEngineGenerationPayload(<IGenerationPayloadType>(
+      this.commandPayload.payload
+    ));
 
-    const projectItemsToGenerateCount = 4; // Derived from CoreTS logic
-    const itemsToGenerateCount =
-      projectItemsToGenerateCount +
-      payload.pages.length +
-      payload.services.length;
-    let generatedItemsCount = 0;
-
-    connection.on(CONSTANTS.API.GEN_LIVE_MESSAGE_TRIGGER_NAME, genMessage => {
-      generatedItemsCount++;
-      const messageWithProgress = `(${generatedItemsCount}/${itemsToGenerateCount}) ${genMessage}`;
-      this.commandPayload.liveMessageHandler(messageWithProgress);
-    });
+    connection.on(
+      CONSTANTS.API.GEN_LIVE_MESSAGE_TRIGGER_NAME,
+      this.commandPayload.liveMessageHandler
+    );
 
     const result = await connection
       .invoke(CONSTANTS.API.SIGNALR_API_GENERATE_METHOD_NAME, body)
