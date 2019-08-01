19f9f7300585eaa778039820ea13bc711d898501
ali-kha <51759191+ali-kha@users.noreply.github.com>
2019-08-01 09:05:16 -0700

    updated to use shorthands (#984)

--- templates/Web/Pages/Vue.List/src/components/ListItem.vue
+++ templates/Web/Pages/Vue.List/src/components/ListItem.vue
@@ -10,7 +10,7 @@
           class="close py-2"
           data-dismiss="alert"
           aria-label="Close"
-          v-on:click="$emit('onDeleteListItem', listItem)"
+          @click="$emit('onDeleteListItem', listItem)"
         >
           <div aria-hidden="true">&times;</div>
         </button>
