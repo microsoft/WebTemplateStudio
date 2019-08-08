18eb1cb88197f9f300cd2f0f719a05efea6fb9d1
Jasmin Goh <t-ligoh@microsoft.com>
2019-08-08 11:33:33 -0700

    removed licence

--- src/extension/src/azure/utils/settings.ts
+++ src/extension/src/azure/utils/settings.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
 import * as fse from "fs-extra";
 import * as path from "path";
 import { CONSTANTS } from "../../constants";
