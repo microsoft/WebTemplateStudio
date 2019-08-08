3837af891451ef73c918a92d4e155077f8ecd952
ali-kha <t-sekham@microsoft.com>
2019-08-08 14:59:39 -0700

    added form validation

--- templates/Web/Pages/Vue.List/src/components/ListForm.vue
+++ templates/Web/Pages/Vue.List/src/components/ListForm.vue
@@ -1,25 +1,59 @@
 ﻿<template>
   <form class="input-group my-3" @submit.prevent="$emit('onAddListItem')">
-    <input
-      :value="value"
-      @input="$emit('input', $event.target.value)"
-      type="text"
-      class="form-control"
-      placeholder="Add text here..."
-      aria-label="Add text here..."
-    />
-    <button type="submit" class="btn btn-primary ml-2">Submit</button>
+    <div class="col-11">
+      <input
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+        type="text"
+        class="form-control"
+        placeholder="Add text here..."
+        aria-label="Add text here..."
+      />
+      <b-alert
+        variant="danger"
+        :show="!this.isValid"
+        dismissible
+      >Input must be at least 1 character long.</b-alert>
+    </div>
+    <span class="nput-group-btn col-1">
+      <button type="submit" class="btn btn-primary" :disabled="!this.isSubmitable">Submit</button>
+    </span>
   </form>
 </template>
 
 <script>
 export default {
   name: "ListForm",
+
   props: {
     value: {
       type: String,
       required: true
     }
+  },
+
+  data() {
+    return {
+      isValid: true,
+      isSubmitable: false
+    };
+  },
+
+  created() {
+    this.$on("input", val => {
+      this.isValid = this.isSubmitable = val.length > 0;
+    });
+    this.$on("onAddListItem", () => {
+      this.isValid = true;
+      this.isSubmitable = false;
+    });
   }
 };
 </script>
+
+<style scoped>
+.col-11 {
+  padding-left: 0px;
+  padding-right: 0px;
+}
+</style>
