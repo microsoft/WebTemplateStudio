5f3dd9c35ee7c458a0217a19803d5ff48b7bb748
Annie Liang <t-anlian@microsoft.com>
2019-08-15 21:49:16 -0700

    fixed accessibiity issue

--- src/client/src/containers/Footer/index.tsx
+++ src/client/src/containers/Footer/index.tsx
@@ -261,6 +261,7 @@ class Footer extends React.Component<Props> {
             <div className={styles.buttonContainer}>
               {pathname !== ROUTES.NEW_PROJECT && (
                 <Link
+                  id="footerBackNavigation"
                   tabIndex={0}
                   className={classnames(
                     buttonStyles.buttonDark,
@@ -282,6 +283,7 @@ class Footer extends React.Component<Props> {
               )}
               {pathname !== ROUTES.REVIEW_AND_GENERATE && (
                 <Link
+                  id="footerNextNavigation"
                   tabIndex={isValidNameAndProjectPath ? 0 : -1}
                   className={classnames(
                     styles.button,
