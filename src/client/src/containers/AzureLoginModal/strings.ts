c19fd835ec01ee5f7b38bd65108105f5eba99d9b
Tanya0609 <tanyi3025@gmail.com>
2019-08-21 11:56:26 -0700

    fix the strings

--- src/client/src/containers/AzureLoginModal/strings.ts
+++ src/client/src/containers/AzureLoginModal/strings.ts
@@ -10,20 +10,20 @@ export const strings = defineMessages({
         id: "azureLoginModal.freeAccountQuestion"
     },
     freeTrialUpgradeAnswer: {
-        defaultMessage: "TODO",
-        id: "azureLoginModal.freeTrialAnswer"
+        defaultMessage: "After 12 months, any of these products you may be using will continue to run, and you’ll be billed at the standard pay-as-you-go rates.",
+        id: "azureLoginModal.freeTrialUpgradeAnswer"
     },
     freeTrialUpgradeQuestion: {
         defaultMessage: "What happens if I want to upgrade my free trial?",
-        id: "azureLoginModal.freeTrialQuestion"
+        id: "azureLoginModal.freeTrialUpgradeQuestion"
     },
     freeTrialAnswer: {
         defaultMessage: "We’ll notify you so you can decide if you want to upgrade to pay-as-you-go pricing and remove the spending limit. If you do, you’ll have access to all the free products. If you don’t, your account and products will be disabled, and you'll need to upgrade to resume usage.",
-        id: "azureLoginModal.freeTrialUpgradeAnswer"
+        id: "azureLoginModal.freeTrialAnswer"
     },
     freeTrialQuestion: {
         defaultMessage: "Do I get charged once I finish my free trial?",
-        id: "azureLoginModal.freeTrialUpgradeQuestion"
+        id: "azureLoginModal.freeTrialQuestion"
     },
     getStartedWithAzure: {
         defaultMessage: "Get Started with Azure",
