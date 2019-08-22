a3f56a7248176ab725d31620f5379673b45b9911
Jasmin Goh <t-ligoh@microsoft.com>
2019-08-22 13:46:01 -0700

    merge with dev

--- src/client/src/containers/AzureLogin/index.tsx
+++ src/client/src/containers/AzureLogin/index.tsx
@@ -84,7 +84,6 @@ class AzureLogin extends React.Component<Props> {
         >
           <div className={styles.logInInfoBar}>
             <Title>{intl.formatMessage(azureMessages.azureLoginTitle)}</Title>
-
           </div>
         </div>
         <AzureStudent />
@@ -94,41 +93,39 @@ class AzureLogin extends React.Component<Props> {
             [styles.signedIn]: isLoggedIn
           })}
         >
-
-            {isLoggedIn && (
-              <div className={styles.azureProfile}>
-                <div className={styles.profileName}>{email}</div>
-                <div
-                  role="button"
-                  tabIndex={0}
-                  className={classnames(styles.button, styles.signOutButton)}
-                  onClick={this.signOutClick}
-                  onKeyDown={this.signOutkeyDownHandler}
-                >
-                  <FormattedMessage
-                    id="header.signOut"
-                    defaultMessage="Sign out"
-                  />
-                </div>
-              </div>
-            )}
-            {!isLoggedIn && (
+          {isLoggedIn && (
+            <div className={styles.azureProfile}>
+              <div className={styles.profileName}>{email}</div>
               <div
                 role="button"
                 tabIndex={0}
-                className={classnames(styles.loginButton, styles.azureProfile)}
-                onClick={openAzureLoginModal}
-                onKeyDown={this.signInKeyDownHandler}
+                className={classnames(styles.button, styles.signOutButton)}
+                onClick={this.signOutClick}
+                onKeyDown={this.signOutkeyDownHandler}
               >
                 <FormattedMessage
-                  id="header.signIn"
-                  defaultMessage="Log In / Create an Account"
+                  id="header.signOut"
+                  defaultMessage="Sign out"
                 />
               </div>
-            )}
-          </div>
-          <AzureSubscriptions />
+            </div>
+          )}
+          {!isLoggedIn && (
+            <div
+              role="button"
+              tabIndex={0}
+              className={classnames(styles.loginButton, styles.azureProfile)}
+              onClick={openAzureLoginModal}
+              onKeyDown={this.signInKeyDownHandler}
+            >
+              <FormattedMessage
+                id="header.signIn"
+                defaultMessage="Log In / Create an Account"
+              />
+            </div>
+          )}
         </div>
+        <AzureSubscriptions />
       </div>
     );
   }
