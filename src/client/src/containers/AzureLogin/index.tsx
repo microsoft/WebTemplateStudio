30d14d6cf81cf37f9427f0fe78ddfc604a25dcc3
Jasmin Goh <t-ligoh@microsoft.com>
2019-08-22 12:47:14 -0700

    Revert "revert merge"
    This reverts commit 89e1e31f97971aa5c01369876f4ac72c89eb2d31.

--- src/client/src/containers/AzureLogin/index.tsx
+++ src/client/src/containers/AzureLogin/index.tsx
@@ -1,41 +1,32 @@
 import * as React from "react";
 import { withRouter, RouteComponentProps, Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { ThunkDispatch } from "redux-thunk";
 import classnames from "classnames";
-
-import LoginCard from "../../components/LoginCard";
-import Title from "../../components/Title";
-
-import azure from "../../assets/azure.svg";
+import { InjectedIntlProps, injectIntl, FormattedMessage } from "react-intl";
+import * as ModalActions from "../../actions/modalActions/modalActions";
 import styles from "./styles.module.css";
 
-import AzureSubscriptions from "../AzureSubscriptions";
 import {
   EXTENSION_COMMANDS,
   EXTENSION_MODULES,
   KEY_EVENTS,
   ROUTES
 } from "../../utils/constants";
-import { InjectedIntlProps, injectIntl, FormattedMessage } from "react-intl";
+
 import { setDetailPageAction } from "../../actions/wizardInfoActions/setDetailsPage";
 import { IOption } from "../../types/option";
 import { azureMessages } from "../../mockData/azureServiceOptions";
-
-import { microsoftAzureDetails } from "../../mockData/azureServiceOptions";
-import { withLocalPath } from "../../utils/getSvgUrl";
 import { AppState } from "../../reducers";
-<<<<<<< HEAD
 import AzureSubscriptions from "../AzureSubscriptions";
 import AzureStudent from "../AzureStudent";
 import Title from "../../components/Title";
-=======
-import { Dispatch } from "redux";
->>>>>>> parent of 87c764b6... Merge branch 'garage/t-yitan/sign-in-modal' into garage/t-ligoh/azure-card
 import RootAction from "../../actions/ActionType";
 import keyUpHandler from "../../utils/keyUpHandler";
 
 interface IDispatchProps {
   setDetailPage: (detailPageInfo: IOption) => any;
+  openAzureLoginModal: () => any;
 }
 
 interface IAzureLoginProps {
@@ -50,15 +41,6 @@ type Props = IDispatchProps &
   RouteComponentProps;
 
 class AzureLogin extends React.Component<Props> {
-  handleClick = () => {
-    // initiates a login command to VSCode ReactPanel class
-
-    this.props.vscode.postMessage({
-      module: EXTENSION_MODULES.AZURE,
-      command: EXTENSION_COMMANDS.AZURE_LOGIN,
-      track: true
-    });
-  };
   signOutClick = () => {
     this.props.vscode.postMessage({
       module: EXTENSION_MODULES.AZURE,
@@ -66,17 +48,25 @@ class AzureLogin extends React.Component<Props> {
       track: true
     });
   };
-  keyDownClick = (event: React.KeyboardEvent) => {
+  signOutkeyDownHandler = (event: React.KeyboardEvent) => {
     if (event.key === KEY_EVENTS.ENTER || event.key === KEY_EVENTS.SPACE) {
       this.signOutClick();
     }
   };
+
+  signInKeyDownHandler = (event: React.KeyboardEvent) => {
+    if (event.key === KEY_EVENTS.ENTER || event.key === KEY_EVENTS.SPACE) {
+      event.preventDefault();
+      event.stopPropagation();
+      this.props.openAzureLoginModal();
+    }
+  };
+
   public render() {
-    const { isLoggedIn, intl, setDetailPage, email } = this.props;
+    const { isLoggedIn, intl, email, openAzureLoginModal } = this.props;
 
     return (
       <div className={styles.centerViewAzure}>
-<<<<<<< HEAD
         {/* Optional Info Banner */}
         <Link
           tabIndex={0}
@@ -87,24 +77,11 @@ class AzureLogin extends React.Component<Props> {
           {azureMessages.azureSkipButton.defaultMessage}
         </Link>
         {/* Add Cloud Services and Login */}
-=======
-        {!isLoggedIn && (
-          <Link
-            tabIndex={0}
-            to={ROUTES.REVIEW_AND_GENERATE}
-            className={styles.optionalFlag}
-            onKeyUp={keyUpHandler}
-          >
-            {azureMessages.azureSkipButton.defaultMessage}
-          </Link>
-        )}
->>>>>>> parent of 87c764b6... Merge branch 'garage/t-yitan/sign-in-modal' into garage/t-ligoh/azure-card
         <div
           className={classnames(styles.header, {
             [styles.signedIn]: isLoggedIn
           })}
         >
-<<<<<<< HEAD
           <div className={styles.logInInfoBar}>
             <Title>{intl.formatMessage(azureMessages.azureLoginTitle)}</Title>
           </div>
@@ -116,40 +93,6 @@ class AzureLogin extends React.Component<Props> {
             [styles.signedIn]: isLoggedIn
           })}
         >
-=======
-          {isLoggedIn && (
-            <div className={styles.azureProfile}>
-              <div className={styles.profileName}>{email}</div>
-              <div
-                role="button"
-                className={styles.button}
-                tabIndex={0}
-                onClick={this.signOutClick.bind(this)}
-                onKeyDown={this.keyDownClick.bind(this)}
-              >
-                <FormattedMessage
-                  id="header.signOut"
-                  defaultMessage="Sign out"
-                />
-              </div>
-            </div>
-          )}
-          <Title>{intl.formatMessage(azureMessages.azureLoginTitle)}</Title>
-          <div className={styles.loginCard}>
-            {!isLoggedIn && (
-              <LoginCard
-                svgUrl={withLocalPath(azure)}
-                handleClick={() => {
-                  this.handleClick();
-                }}
-                cardTitle={intl.formatMessage(azureMessages.azureTitle)}
-                cardBody={intl.formatMessage(azureMessages.azureCardBody)}
-                handleDetailsClick={setDetailPage}
-                option={microsoftAzureDetails}
-              />
-            )}
-          </div>
->>>>>>> parent of 87c764b6... Merge branch 'garage/t-yitan/sign-in-modal' into garage/t-ligoh/azure-card
           <AzureSubscriptions />
         </div>
       </div>
@@ -169,8 +112,11 @@ const mapStateToProps = (state: AppState): IAzureLoginProps => {
 };
 
 const mapDispatchToProps = (
-  dispatch: Dispatch<RootAction>
+  dispatch: ThunkDispatch<AppState, void, RootAction>
 ): IDispatchProps => ({
+  openAzureLoginModal: () => {
+    dispatch(ModalActions.openAzureLoginModalAction());
+  },
   setDetailPage: (detailPageInfo: IOption) => {
     const isIntlFormatted = true;
     dispatch(setDetailPageAction(detailPageInfo, isIntlFormatted));
