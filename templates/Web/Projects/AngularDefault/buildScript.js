53084c748e4cd2ff2bc9223add6bbf8504aabd27
mhatvan <markus_hatvan@aon.at>
2019-11-04 19:53:14 +0100

    * add explanatory comment to angular-specific buildScript.js
    Signed-off-by: mhatvan <markus_hatvan@aon.at>

--- templates/Web/Projects/AngularDefault/buildScript.js
+++ templates/Web/Projects/AngularDefault/buildScript.js
@@ -1,3 +1,4 @@
+// this file is used to compile the frontend Angular app and move the output to the server folder for later deployment.
 const fs = require("fs");
 const fse = require("fs-extra");
 const childProcess = require("child_process");
