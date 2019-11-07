43f492bcfe266e7e6c12edbc4db34ce212173240
japarisi <japarisivaldes@gmail.com>
2019-11-07 10:22:09 +0100

    change model for phyton and moleculer

--- templates/Web/Pages/Angular.MasterDetail/src/app/app-shell/Param_SourceName_Kebab/master-detail.model.ts
+++ templates/Web/Pages/Angular.MasterDetail/src/app/app-shell/Param_SourceName_Kebab/master-detail.model.ts
@@ -7,5 +7,5 @@ export interface ISampleOrder {
   orderTotal: number;
   shortDescription: string;
   longDescription: string;
-  imageUrl: string;
+  imageName: string;
 }
--- templates/Web/_composition/NodeJS/Page.Node.AddSampleData.ForAllExceptList/server/sampleData$wts.Page_gpostaction.js
+++ templates/Web/_composition/NodeJS/Page.Node.AddSampleData.ForAllExceptList/server/sampleData$wts.Page_gpostaction.js
@@ -29,7 +29,7 @@ sampleData.textAssets = [
     orderTotal: 2490.0,
     orderDate: new Date(2017, 5, 24).toDateString(),
     id: 1,
-    imageUrl:'GreyAvatar.svg'
+    imageName:'GreyAvatar.svg'
   },
   {
     shortDescription: shortLoremIpsum,
@@ -40,7 +40,7 @@ sampleData.textAssets = [
     orderTotal: 1760.0,
     orderDate: new Date(2017, 5, 24).toDateString(),
     id: 2,
-    imageUrl:'GreyAvatar.svg'
+    imageName:'GreyAvatar.svg'
   },
   {
     shortDescription: shortLoremIpsum,
@@ -51,7 +51,7 @@ sampleData.textAssets = [
     orderTotal: 665.0,
     orderDate: new Date(2017, 6, 3).toDateString(),
     id: 3,
-    imageUrl:'GreyAvatar.svg'
+    imageName:'GreyAvatar.svg'
   },
   {
     shortDescription: shortLoremIpsum,
@@ -62,7 +62,7 @@ sampleData.textAssets = [
     orderTotal: 560.0,
     orderDate: new Date(2017, 6, 5).toDateString(),
     id: 4,
-    imageUrl:'GreyAvatar.svg'
+    imageName:'GreyAvatar.svg'
   },
   {
     shortDescription: shortLoremIpsum,
@@ -73,7 +73,7 @@ sampleData.textAssets = [
     orderTotal: 810.0,
     orderDate: new Date(2017, 6, 7).toDateString(),
     id: 5,
-    imageUrl:'GreyAvatar.svg'
+    imageName:'GreyAvatar.svg'
   },
   {
     shortDescription: shortLoremIpsum,
@@ -84,7 +84,7 @@ sampleData.textAssets = [
     orderTotal: 196.5,
     orderDate: new Date(2017, 6, 7).toDateString(),
     id: 6,
-    imageUrl:'GreyAvatar.svg'
+    imageName:'GreyAvatar.svg'
   },
   {
     shortDescription: shortLoremIpsum,
@@ -95,7 +95,7 @@ sampleData.textAssets = [
     orderTotal: 270.0,
     orderDate: new Date(2017, 6, 11).toDateString(),
     id: 7,
-    imageUrl:'GreyAvatar.svg'
+    imageName:'GreyAvatar.svg'
   },
   {
     shortDescription: shortLoremIpsum,
@@ -106,7 +106,7 @@ sampleData.textAssets = [
     orderTotal: 736.0,
     orderDate: new Date(2017, 6, 14).toDateString(),
     id: 8,
-    imageUrl:'GreyAvatar.svg'
+    imageName:'GreyAvatar.svg'
   },
   {
     shortDescription: shortLoremIpsum,
@@ -117,7 +117,7 @@ sampleData.textAssets = [
     orderTotal: 800.0,
     orderDate: new Date(2017, 6, 18).toDateString(),
     id: 9,
-    imageUrl:'GreyAvatar.svg'
+    imageName:'GreyAvatar.svg'
   }
 ];
 //}]}
