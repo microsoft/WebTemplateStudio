64ca79d8aba03d38c78e90eeec3203a22e1167c4
japarisi <japarisivaldes@gmail.com>
2019-11-20 17:45:31 +0100

    refactor to single test

--- src/client/src/containers/SelectOption/SelectOption.test.tsx
+++ src/client/src/containers/SelectOption/SelectOption.test.tsx
@@ -4,19 +4,19 @@ import { SelectOption } from "./index";
 import { ISelected } from "../../types/selected";
 
 describe("SelectOption", () => {
-  let keyBlank="",
-    keyGrid="",
-    keyList="",
-    keyMaster="";
+  let masterAnyFrameworkBlank="",
+  masterAnyFrameworkGrid="",
+  masterAnyFrameworkList="",
+  masterAnyFrameworkMaster="";
 
-  describe("react", () => {
+  describe("any framework", () => {
     let props: any;
     let wrapper: any;
     let instance: any;
-    keyBlank="wts.Page.React.Blank";
-    keyGrid="wts.Page.React.Grid";
-    keyList="wts.Page.React.List";
-    keyMaster="wts.Page.React.MasterDetail";
+    masterAnyFrameworkBlank="anyFramework.Blank";
+    masterAnyFrameworkGrid="anyFramework.Grid";
+    masterAnyFrameworkList="anyFramework.List";
+    masterAnyFrameworkMaster="anyFramework.MasterDetail";
 
 
     let loadBaseMockComp = ()=>{
@@ -32,7 +32,7 @@ describe("SelectOption", () => {
             author: "Microsoft",
             body: "A blank page for you to build your web application from scratch.",
             defaultName: "Blank",
-            internalName: keyBlank,
+            internalName: masterAnyFrameworkBlank,
             isValidTitle: true,
             licenses: [{}],
             longDescription: "This is the most basic page. A blank canvas to mold into whatever you wish. The blank page leaves pretty much everything up to you.",
@@ -44,7 +44,7 @@ describe("SelectOption", () => {
             author: "Microsoft",
             body: "Simple image and text components which are organized into a grid.",
             defaultName: "Grid",
-            internalName: keyGrid,
+            internalName: masterAnyFrameworkGrid,
             isValidTitle: true,
             licenses: [{}],
             longDescription: "A page displaying simple image and text components which are organized into a grid. Grid pages are a system for creating order among elements in a website.",
@@ -56,7 +56,7 @@ describe("SelectOption", () => {
             author: "Microsoft",
             body: "Add and remove text from an adaptive list.",
             defaultName: "List",
-            internalName: keyList,
+            internalName: masterAnyFrameworkList,
             isValidTitle: true,
             licenses: [{}],
             longDescription: "The list page allows you to add custom text in the form of an adaptive list. This pattern is frequently used for blog pages and messaging apps. If a database is selected from the Azure Cloud Services the list page will automatically connect to the deployed Azure database.",
@@ -68,7 +68,7 @@ describe("SelectOption", () => {
             author: "Microsoft",
             body: "A master pane and a details pane for content.",
             defaultName: "Master Detail",
-            internalName: keyMaster,
+            internalName: masterAnyFrameworkMaster,
             isValidTitle: true,
             licenses: [{}],
             longDescription: "The master-detail page has a master pane and a details pane for content. When an item in the master list is selected, the details pane is updated. This pattern is frequently used for email and address books.",
@@ -104,52 +104,52 @@ describe("SelectOption", () => {
 
     it("on first moment should have 1 page blank", () => {
       expect(props.currentCardData.length).toBe(1);
-      expect(props.cardTypeCount[keyBlank]).toBe(1);
+      expect(props.cardTypeCount[masterAnyFrameworkBlank]).toBe(1);
     });
 
     it("add 1 page blank", () => {
       instance.addPage(0);
-      expect(props.cardTypeCount[keyBlank]).toBe(2);
+      expect(props.cardTypeCount[masterAnyFrameworkBlank]).toBe(2);
     });
 
     it("add 1 page blank", () => {
       instance.addPage(0);
-      expect(props.cardTypeCount[keyBlank]).toBe(3);
+      expect(props.cardTypeCount[masterAnyFrameworkBlank]).toBe(3);
     });
 
     it("remove 1 page blank", () => {
       instance.removePage(0);
-      expect(props.cardTypeCount[keyBlank]).toBe(2);
+      expect(props.cardTypeCount[masterAnyFrameworkBlank]).toBe(2);
     });
 
     it("add 1 grid page", () => {
       instance.addPage(1);
-      expect(props.cardTypeCount[keyGrid]).toBe(1);
+      expect(props.cardTypeCount[masterAnyFrameworkGrid]).toBe(1);
     });
 
     it("add 1 grid page", () => {
       instance.addPage(1);
-      expect(props.cardTypeCount[keyGrid]).toBe(2);
+      expect(props.cardTypeCount[masterAnyFrameworkGrid]).toBe(2);
     });
 
     it("add 1 list page", () => {
       instance.addPage(2);
-      expect(props.cardTypeCount[keyList]).toBe(1);
+      expect(props.cardTypeCount[masterAnyFrameworkList]).toBe(1);
     });
 
     it("add 1 master page", () => {
       instance.addPage(3);
-      expect(props.cardTypeCount[keyMaster]).toBe(1);
+      expect(props.cardTypeCount[masterAnyFrameworkMaster]).toBe(1);
     });
 
     it("remove 1 list page", () => {
       instance.removePage(2);
-      expect(props.cardTypeCount[keyList]).toBe(0);
+      expect(props.cardTypeCount[masterAnyFrameworkList]).toBe(0);
     });
 
     it("remove 1 master page", () => {
       instance.removePage(3);
-      expect(props.cardTypeCount[keyMaster]).toBe(0);
+      expect(props.cardTypeCount[masterAnyFrameworkMaster]).toBe(0);
     });
 
   });
