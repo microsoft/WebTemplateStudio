c27d9c3e8bc4456db133621c009bcb835e0c3a8a
japarisi <japarisivaldes@gmail.com>
2019-12-10 12:02:20 +0100

    Rollback delete libs

--- templates/Web/Projects/NodeDefault/server/server.js
+++ templates/Web/Projects/NodeDefault/server/server.js
@@ -4,6 +4,7 @@
  * Module dependencies.
  */
 
+const debug = require("debug")("WebTemplateStudioExpress:server");
 const http = require("http");
 const app = require("./app");
 const CONSTANTS = require("./constants");
@@ -82,5 +83,5 @@ function onError(error) {
 function onListening() {
   const addr = server.address();
   const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
-  console.log(`Listening on ${bind}`);
+  debug(`Listening on ${bind}`);
 }
