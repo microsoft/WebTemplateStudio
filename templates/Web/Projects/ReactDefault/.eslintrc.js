10bfae0d2b44850e42c70f149492ff8410f24b2c
japarisi <japarisivaldes@gmail.com>
2019-12-20 11:51:12 +0100

    add react rule in lint settings

--- templates/Web/Projects/ReactDefault/.eslintrc.js
+++ templates/Web/Projects/ReactDefault/.eslintrc.js
@@ -25,5 +25,10 @@ module.exports = {
     ],
     "rules": {
     },
-    "parser": "babel-eslint"
+    "parser": "babel-eslint",
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    }
 };
