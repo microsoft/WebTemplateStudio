a72110dc075f107f0a27dd92cc4cd3ae7e31efd6
japarisi <japarisivaldes@gmail.com>
2020-01-20 12:22:58 +0100

    refactor to hooks

--- templates/Web/Projects/ReactDefault/src/App.jsx
+++ templates/Web/Projects/ReactDefault/src/App.jsx
@@ -5,17 +5,15 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 //TODO Web Template Studio: Add routes for your new pages here.
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <NavBar />
-        <Switch>
-        </Switch>
-        <Footer />
-      </React.Fragment>
-    );
-  }
+const App = ()=> {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <Switch>
+      </Switch>
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default App;
