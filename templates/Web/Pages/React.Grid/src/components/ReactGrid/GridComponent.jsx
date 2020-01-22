32cbebb40ee742ec36e926f5f6260cb5fe8ec7ca
japarisi <japarisivaldes@gmail.com>
2020-01-22 12:47:52 +0100

    refactor grid page to hooks

--- templates/Web/Pages/React.Grid/src/components/ReactGrid/GridComponent.jsx
+++ templates/Web/Pages/React.Grid/src/components/ReactGrid/GridComponent.jsx
@@ -3,18 +3,13 @@ import ImgGreyBox from "../../images/GreyBox.svg"
 import PropTypes from "prop-types";
 
 const GridComponent = ({ gridItem }) => {
-  const {header, shortDescription} = gridItem;
   return (
     <div className="col-md-4 col-sm-12 p-5">
       <img src={ImgGreyBox} alt="Default Grey Box" className="mb-3" />
-      <h3>{header}</h3>
-      <p>{shortDescription}</p>
+      <h3>{gridItem.header}</h3>
+      <p>{gridItem.shortDescription}</p>
     </div>
   );
-}
-
-GridComponent.propTypes = {
-  gridItem: PropTypes.any
 }
 
 export default GridComponent;
--- templates/Web/_composition/ReactJS/Page.React.AddImport/src/App_postaction.jsx
+++ templates/Web/_composition/ReactJS/Page.React.AddImport/src/App_postaction.jsx
@@ -1,5 +1,5 @@
-﻿import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+﻿import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
