cbfca533127607eb21ddd0294e6caf018eb7b3ff
dgomezc <v-davgom@microsoft.com>
2020-01-30 13:00:57 +0100

    Force Eslint warning

--- src/extension/.eslintrc.js
+++ src/extension/.eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
     //temporarily disabled until we decide which rules to enable
     "@typescript-eslint/no-explicit-any" : "off",
     "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/no-non-null-assertion" : "off",
+   // "@typescript-eslint/no-non-null-assertion" : "off",
     "@typescript-eslint/no-namespace" : "off",
     "@typescript-eslint/no-var-requires" : "off"
   }
