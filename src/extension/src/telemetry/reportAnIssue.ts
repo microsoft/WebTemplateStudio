daa77a1e5c14bd0e5aa3b9d42c2ab8e4c1d9823e
japarisi <japarisivaldes@gmail.com>
2020-01-31 08:48:18 +0100

    fix import

--- src/extension/src/telemetry/reportAnIssue.ts
+++ src/extension/src/telemetry/reportAnIssue.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import opn = require("opn");
+const opn = require("opn");
 import * as vscode from 'vscode';
 import { getPackageInfo } from "../utils/packageInfo";
 import { IParsedError } from "./parseError";
