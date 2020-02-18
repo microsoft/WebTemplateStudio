cd16aeec7bacf3d4f60fdec6fd919f603b5d2990
japarisi <japarisivaldes@gmail.com>
2020-02-18 14:17:10 +0100

    fix load frameworks on wizard option

--- src/client/src/containers/PageSelectFrameworks/index.tsx
+++ src/client/src/containers/PageSelectFrameworks/index.tsx
@@ -3,54 +3,28 @@ import { connect } from "react-redux";
 
 import {
   EXTENSION_MODULES,
-  EXTENSION_COMMANDS,
-  FRAMEWORK_TYPE
+  EXTENSION_COMMANDS
 } from "../../utils/constants";
 
 
 import { IStateProps, IDispatchProps } from "./interfaces";
-import {mapStateToProps, mapDispatchToProps} from "./store";
+import {mapStateToProps} from "./store";
 import FrameworkCard from "./FrameworkCard";
 import styles from "./styles.module.css";
-import { getFrameworks } from "../../utils/extensionService/extensionService";
-import { parseFrameworksPayload } from "../../utils/parseFrameworksPayload";
-import messages from "./messages";
 import { InjectedIntlProps, injectIntl } from "react-intl";
+import messages from "./messages";
 
 type Props = IStateProps & IDispatchProps & InjectedIntlProps;
 
-class SelectFrameworks extends React.Component<Props> {
+const SelectFrameworks = (props:Props) => {
+  const { frontendOptions, backendOptions, intl } = props;
 
-  componentDidMount(){
-    const { frontendOptions } = this.props;
-    this.getDependencyInfoAndSetToStore();
-    if (frontendOptions.length ===0) this.getFrameworksListAndSetToStore();
-  }
+  React.useEffect(()=>{
+    getDependencyInfoAndSetToStore();
+  },[]);
 
-  getFrameworksListAndSetToStore (){
-    const { vscode, isPreview, setFrontendFrameworks, setBackendFrameworks } = this.props;
-
-    getFrameworks(vscode, isPreview).then((event:any)=>{
-      let message = event.data;
-      setFrontendFrameworks(
-        parseFrameworksPayload(
-          message.payload.frameworks,
-          FRAMEWORK_TYPE.FRONTEND,
-          message.payload.isPreview
-        )
-      );
-      setBackendFrameworks(
-        parseFrameworksPayload(
-          message.payload.frameworks,
-          FRAMEWORK_TYPE.BACKEND,
-          message.payload.isPreview
-        )
-      );
-    });
-  }
-
-  getDependencyInfoAndSetToStore(){
-    const { vscode } = this.props;
+  const getDependencyInfoAndSetToStore = () =>{
+    const { vscode } = props;
     // send messages to extension to check dependency info when this component loads
     vscode.postMessage({
       module: EXTENSION_MODULES.DEPENDENCYCHECKER,
@@ -68,29 +42,26 @@ class SelectFrameworks extends React.Component<Props> {
     });
   }
 
-  public render() {
-    const { frontendOptions, backendOptions, intl } = this.props;
-    return (
-      <div>
-        <h1 className={styles.title}>{intl.formatMessage(messages.frontendTitle)}</h1>
-        <div className={styles.flexContainer}>
-          {frontendOptions.map((framework) => {
-            return (
-              <FrameworkCard framework={framework} isFrontEnd={true}/>
-            );
-          })}
-        </div>
-        <h1 className={styles.title}>{intl.formatMessage(messages.backendTitle)}</h1>
-        <div className={styles.flexContainer}>
-          {backendOptions.map((framework) => {
-            return (
-              <FrameworkCard framework={framework} isFrontEnd={false}/>
-            );
-          })}
-        </div>
+  return (
+    <div>
+      <h1 className={styles.title}>{intl.formatMessage(messages.frontendTitle)}</h1>
+      <div className={styles.flexContainer}>
+        {frontendOptions.map((framework) => {
+          return (
+            <FrameworkCard framework={framework} isFrontEnd={true}/>
+          );
+        })}
       </div>
-    );
-  }
+      <h1 className={styles.title}>{intl.formatMessage(messages.backendTitle)}</h1>
+      <div className={styles.flexContainer}>
+        {backendOptions.map((framework) => {
+          return (
+            <FrameworkCard framework={framework} isFrontEnd={false}/>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(injectIntl(SelectFrameworks));
+export default connect(mapStateToProps)(injectIntl(SelectFrameworks));
