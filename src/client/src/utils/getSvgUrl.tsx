aa112c4be48552192f363bba035dd997439a7709
japarisi <japarisivaldes@gmail.com>
2020-03-02 11:51:21 +0100

    refactor svg imports to svg chunks

--- src/client/src/utils/getSvgUrl.tsx
+++ src/client/src/utils/getSvgUrl.tsx
@@ -1,146 +1,194 @@
 import * as React from "react";
 import classnames from "classnames";
-
-import { ReactComponent as FullStack } from "../assets/fullstack.svg";
-import { ReactComponent as MasterDetail } from "../assets/masterdetail.svg";
-import { ReactComponent as BlankPage } from "../assets/blankpage.svg";
-import { ReactComponent as ContentGrid } from "../assets/contentgrid.svg";
-import { ReactComponent as List } from "../assets/list.svg";
-
-import svgStyles from "./svgStyles.module.css";
-
+import Loadable from "react-loadable";
 import { WIZARD_CONTENT_INTERNAL_NAMES } from "./constants";
-import { ReactComponent as ReactIcon } from "../assets/react.svg";
-import { ReactComponent as AngularIcon } from "../assets/angular.svg";
-import { ReactComponent as VueIcon } from "../assets/vue.svg";
-import { ReactComponent as NodeIcon } from "../assets/node.svg";
-import { ReactComponent as MoleculerIcon } from "../assets/moleculer.svg";
-import { ReactComponent as FlaskIcon } from "../assets/flask.svg";
-import { ReactComponent as AzureIcon } from "../assets/azure.svg";
-import { ReactComponent as AppserviceIcon } from "../assets/appservice.svg";
-import { ReactComponent as AzurefunctionsIcon } from "../assets/azurefunctions.svg";
 
-import { ReactComponent as CosmosdbIcon } from "../assets/cosmosdb.svg";
 import warning from "../assets/warning.svg";
 import cancel from "../assets/cancel.svg";
 import greencheck from "../assets/checkgreen.svg";
 
-import { ReactComponent as Masterdetailscreenshot }  from "../assets/masterdetailscreenshot.svg";
-import { ReactComponent as Listscreenshot } from "../assets/listscreenshot.svg";
-import { ReactComponent as Gridscreenshot }  from "../assets/gridscreenshot.svg";
-import { ReactComponent as Blankscreenshot } from "../assets/blankscreenshot.svg";
+const FullStack = Loadable({
+  loader: () => import(/* webpackChunkName: "FullStack" */  "./svgComponents/FullStack"),
+  loading:() => <div/>
+});
+const MasterDetail = Loadable({
+  loader: () => import(/* webpackChunkName: "MasterDetail" */  "./svgComponents/MasterDetail"),
+  loading:() => <div/>
+});
+const BlankPage = Loadable({
+  loader: () => import(/* webpackChunkName: "BlankPage" */  "./svgComponents/BlankPage"),
+  loading:() => <div/>
+});
+const ContentGrid = Loadable({
+  loader: () => import(/* webpackChunkName: "ContentGrid" */  "./svgComponents/ContentGrid"),
+  loading:() => <div/>
+});
+const Masterdetailscreenshot = Loadable({
+  loader: () => import(/* webpackChunkName: "Masterdetailscreenshot" */  "./svgComponents/Masterdetailscreenshot"),
+  loading:() => <div/>
+});
+const Listscreenshot = Loadable({
+  loader: () => import(/* webpackChunkName: "Listscreenshot" */  "./svgComponents/Listscreenshot"),
+  loading:() => <div/>
+});
+const Gridscreenshot = Loadable({
+  loader: () => import(/* webpackChunkName: "Gridscreenshot" */  "./svgComponents/Gridscreenshot"),
+  loading:() => <div/>
+});
+const Blankscreenshot = Loadable({
+  loader: () => import(/* webpackChunkName: "Blankscreenshot" */  "./svgComponents/Blankscreenshot"),
+  loading:() => <div/>
+});
+const List = Loadable({
+  loader: () => import(/* webpackChunkName: "List" */  "./svgComponents/List"),
+  loading:() => <div/>
+});
+//icons
+const ReactIcon = Loadable({
+  loader: () => import(/* webpackChunkName: "ReactIcon" */  "./svgComponents/ReactIcon"),
+  loading:() => <div/>
+});
+const AngularIcon = Loadable({
+  loader: () => import(/* webpackChunkName: "AngularIcon" */  "./svgComponents/AngularIcon"),
+  loading:() => <div/>
+});
+const VueIcon = Loadable({
+  loader: () => import(/* webpackChunkName: "VueIcon" */  "./svgComponents/VueIcon"),
+  loading:() => <div/>
+});
+const NodeIcon = Loadable({
+  loader: () => import(/* webpackChunkName: "NodeIcon" */  "./svgComponents/NodeIcon"),
+  loading:() => <div/>
+});
+const FlaskIcon = Loadable({
+  loader: () => import(/* webpackChunkName: "FlaskIcon" */  "./svgComponents/FlaskIcon"),
+  loading:() => <div/>
+});
+const MoleculerIcon = Loadable({
+  loader: () => import(/* webpackChunkName: "MoleculerIcon" */  "./svgComponents/MoleculerIcon"),
+  loading:() => <div/>
+});
+const AzureIcon = Loadable({
+  loader: () => import(/* webpackChunkName: "AzureIcon" */  "./svgComponents/AzureIcon"),
+  loading:() => <div/>
+});
+const AppserviceIcon = Loadable({
+  loader: () => import(/* webpackChunkName: "AppServiceIcon" */  "./svgComponents/AppserviceIcon"),
+  loading:() => <div/>
+});
+const CosmosdbIcon = Loadable({
+  loader: () => import(/* webpackChunkName: "CosmosdbIcon" */  "./svgComponents/CosmosdbIcon"),
+  loading:() => <div/>
+});
 
 const SVG_MAPPINGS = {
   [WIZARD_CONTENT_INTERNAL_NAMES.REACT]: (style: string) => (
-    <ReactIcon className={classnames(style)}/>
+    <ReactIcon style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.ANGULAR]: (style: string) => (
-    <AngularIcon className={classnames(style)}/>
+    <AngularIcon style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.VUE]: (style: string) => (
-    <VueIcon className={classnames(style)}/>
+    <VueIcon style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.NODE]: (style: string) => (
-    <NodeIcon className={classnames(style)}/>
+    <NodeIcon style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.MOLECULER]: (style: string) => (
-    <MoleculerIcon className={classnames(style)}/>
+    <MoleculerIcon style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.FLASK]: (style: string) => (
-    <FlaskIcon className={classnames(style)}/>
+    <FlaskIcon style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.APP_SERVICE]: (style: string) => (
-    <AppserviceIcon className={classnames(style)}/>
-  ),
-  [WIZARD_CONTENT_INTERNAL_NAMES.AZURE_FUNCTIONS]: (style: string) => (
-    <AzurefunctionsIcon className={classnames(style)}/>
+    <AppserviceIcon style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.AZURE]: (style: string) => (
-    <AzureIcon className={classnames(style)}/>
+    <AzureIcon style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.COSMOS_DB]: (style: string) => (
-    <CosmosdbIcon className={classnames(style)}/>
+    <CosmosdbIcon style={style}/>
   )
 };
 
 const SVG_REACTCOMPONENT_MAPPINGS = {
   [WIZARD_CONTENT_INTERNAL_NAMES.FULL_STACK_APP]: (style: string) => (
-    <FullStack className={classnames(style, svgStyles.icon)} />
+    <FullStack style={style} />
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.REACT_MASTER_DETAIL]: (style: string) => (
-    <MasterDetail className={classnames(style, svgStyles.icon)} />
+    <MasterDetail style={style} />
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.REACT_BLANK_PAGE]: (style: string) => (
-    <BlankPage className={classnames(style, svgStyles.icon)} />
+    <BlankPage style={style} />
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.REACT_CONTENT_GRID]: (style: string) => (
-    <ContentGrid className={classnames(style, svgStyles.icon)} />
+    <ContentGrid style={style} />
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.REACT_LIST]: (style: string) => (
-    <List className={classnames(style, svgStyles.icon)} />
+    <List style={style} />
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.ANGULAR_MASTER_DETAIL]: (style: string) => (
-    <MasterDetail className={classnames(style, svgStyles.icon)} />
+    <MasterDetail style={style} />
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.ANGULAR_BLANK_PAGE]: (style: string) => (
-    <BlankPage className={classnames(style, svgStyles.icon)} />
+    <BlankPage style={style} />
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.ANGULAR_CONTENT_GRID]: (style: string) => (
-    <ContentGrid className={classnames(style, svgStyles.icon)} />
+    <ContentGrid style={style} />
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.ANGULAR_LIST]: (style: string) => (
-    <List className={classnames(style, svgStyles.icon)} />
+    <List style={style} />
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.VUE_MASTER_DETAIL]: (style: string) => (
-    <MasterDetail className={classnames(style, svgStyles.icon)} />
+    <MasterDetail style={style} />
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.VUE_BLANK_PAGE]: (style: string) => (
-    <BlankPage className={classnames(style, svgStyles.icon)} />
+    <BlankPage style={style} />
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.VUE_CONTENT_GRID]: (style: string) => (
-    <ContentGrid className={classnames(style, svgStyles.icon)} />
+    <ContentGrid style={style} />
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.VUE_LIST]: (style: string) => (
-    <List className={classnames(style, svgStyles.icon)} />
+    <List style={style} />
   )
 };
 
 const SVG_SCREENSHOT_MAPPINGS = {
   [WIZARD_CONTENT_INTERNAL_NAMES.REACT_MASTER_DETAIL]: (style: string) => (
-    <Masterdetailscreenshot className={classnames(style)} />
+    <Masterdetailscreenshot style={style} />
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.ANGULAR_MASTER_DETAIL]: (style: string) => (
-    <Masterdetailscreenshot className={classnames(style)}/>
+    <Masterdetailscreenshot style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.VUE_MASTER_DETAIL]: (style: string) => (
-    <Masterdetailscreenshot  className={classnames(style)}/>
+    <Masterdetailscreenshot style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.REACT_CONTENT_GRID]: (style: string) => (
-    <Gridscreenshot className={classnames(style)}/>
+    <Gridscreenshot style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.ANGULAR_CONTENT_GRID]: (style: string) => (
-    <Gridscreenshot className={classnames(style)}/>
+    <Gridscreenshot style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.VUE_CONTENT_GRID]: (style: string) => (
-    <Gridscreenshot className={classnames(style)}/>
+    <Gridscreenshot style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.REACT_LIST]: (style: string) => (
-    <Listscreenshot className={classnames(style)}/>
+    <Listscreenshot style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.ANGULAR_LIST]: (style: string) => (
-    <Listscreenshot className={classnames(style)}/>
+    <Listscreenshot style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.VUE_LIST]: (style: string) => (
-    <Listscreenshot className={classnames(style)}/>
+    <Listscreenshot style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.REACT_BLANK_PAGE]: (style: string) => (
-    <Blankscreenshot className={classnames(style)}/>
+    <Blankscreenshot style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.ANGULAR_BLANK_PAGE]: (style: string) => (
-    <Blankscreenshot className={classnames(style)}/>
+    <Blankscreenshot style={style}/>
   ),
   [WIZARD_CONTENT_INTERNAL_NAMES.VUE_BLANK_PAGE]: (style: string) => (
-    <Blankscreenshot className={classnames(style)}/>
+    <Blankscreenshot style={style}/>
   )
 };
 
