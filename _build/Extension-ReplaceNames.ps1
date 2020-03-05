ece4980825c71920c5ea6b6c3bce2b7d1390bf13
sibille <sibille@ilitia.com>
2020-03-05 14:13:13 +0100

    Replace names in extensions package json

---
+++ _build/Extension-ReplaceNames.ps1
@@ -0,0 +1,47 @@
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True,Position=1)]
+  [string]$vsixPackageJson,
+
+  [Parameter(Mandatory=$True,Position=2)]
+  [string]$vsixName,
+
+  [Parameter(Mandatory=$True,Position=3)]
+  [string]$vsixDisplayName,
+
+  [Parameter(Mandatory=$True,Position=4)]
+  [string]$versionNumber
+)
+function Clear-WhiteSpace ($Text) {
+  "$($Text -replace "(`t|`n|`r)"," " -replace "\s+"," ")".Trim()
+}
+
+## SET NAME AND VERSION IN VSIX Package json
+if($vsixName){
+  Write-Host
+  Write-Host "Setting name and version in VSIX package json"
+
+  if(Test-Path($vsixPackageJson)){
+    $packagejsonContent = Get-Content $vsixPackageJson | ConvertFrom-Json
+    $LocalIdentity = $packagejsonContent.name
+    $localDisplayName = $packagejsonContent.displayName
+    $localVersionNumber = $packagejsonContent.version
+
+    $content = (Get-Content -path $vsixPackageJson -Raw)
+
+    $content = $content -replace "$LocalIdentity" , "$vsixName"
+    $content = $content -replace "$localDisplayName" , "$vsixDisplayName"
+    $content = $content -replace "$localVersionNumber" , "$versionNumber"
+
+    $content | Set-Content -Path $vsixPackageJson
+
+    Write-Host "$resolvedPath - Version, Name & DisplayName applied ($versionNumber, $vsixIdentity, $vsixDisplayName)"
+  }
+  else{
+    throw "No VSIX package json file found."
+  }
+}
+else{
+  throw "Identity is mandatory."
+}
+
