bcb9857047da137bf10486d7dd888a4fe42d0fb1
japarisi <japarisivaldes@gmail.com>
2020-03-12 03:35:12 +0100

    clean code

--- src/client/src/components/DraggableSidebarItem2/index.tsx
+++ src/client/src/components/DraggableSidebarItem2/index.tsx
@@ -42,24 +42,14 @@ const Reorder = Loadable({
  */
 
 interface IStateProps {
-  page: ISelected;
   text?: string;
   cosmosDB?: boolean;
   appService?: boolean;
-  reorderSvgUrl?: string;
-  pageSvgUrl?: string;
-  itemTitle?: string;
-  handleInputChange?: (e: any, idx: number) => void;
-  maxInputLength?: number;
   idx?: number;
-  azureFunctionName?: IFunctionName;
   withIndent?: boolean;
-  withLargeIndent?: boolean;
   handleCloseClick?: (idx: number) => void;
   intl: InjectedIntl;
   customInputStyle?: string;
-  isAzureFunction?: boolean;
-  totalCount?: number;
 }
 
 interface ISortablePageListProps {
@@ -74,23 +64,14 @@ interface ISortableDispatchProps {
 type Props = IStateProps & ISortablePageListProps & InjectedIntlProps & ISortableDispatchProps;
 
 const DraggableSidebarItem = ({
-  page,
   text,
   cosmosDB,
   appService,
-  pageSvgUrl,
-  reorderSvgUrl,
-  itemTitle,
-  maxInputLength,
   idx,
-  azureFunctionName,
   withIndent,
-  withLargeIndent,
   handleCloseClick,
   intl,
   customInputStyle,
-  isAzureFunction,
-  totalCount,
   updatePage,
   validations,
   selectedPages
@@ -105,75 +86,23 @@ const DraggableSidebarItem = ({
     idx && handleCloseClick && handleCloseClick(idx - 1); // correction for idx + 1 to prevent 0th falsey behaviour
   };
 
-  const [validValue, setValidValue] = React.useState<string>(page ? page.title:"");
-  const inputRef = React.createRef<HTMLInputElement>();
-  /*const setFocus = () =>{
-      const node = inputRef.current!
-      node.focus();
-  }
-  const setSelect = () =>{
-    const node = inputRef.current!
-    node.select();
-  }*/
-
-  const handleInputChange = async (newTitle: string, idx: number, isDirty: boolean) => {
-    page.title = newTitle;
-    const validationResult = await validateItemName(newTitle, validations.itemNameValidationConfig, selectedPages);
-    page.error = validationResult.error;
-    page.isValidTitle = validationResult.isValid;
-    page.isDirty=isDirty
-    updatePage(page);
-  };
-
   return (
     <div>
       <div className={styles.draggablePage}>
         <div className={styles.iconContainer}>
-          {!(withIndent || withLargeIndent) && (
-            <Reorder style={styles.reorderIcon} />
-          )}
           {cosmosDB && <CosmosDBIcon style={styles.reorderIcon}/>}
           {appService && <AppServiceIcon style={styles.reorderIcon}/>}
         </div>
         <div className={styles.errorStack}>
           <div
             className={classnames(customInputStyle, {
-              [styles.pagesTextContainer]: pageSvgUrl!=="",
-              [styles.textContainer]: !withIndent,
-              [styles.largeIndentContainer]: withLargeIndent
+              [styles.pagesTextContainer]: true,
+              [styles.textContainer]: true,
+              [styles.largeIndentContainer]: true
             })}
           >
             <div className={styles.inputContainer}>
-              {pageSvgUrl &&
-                (getSvg(page!.internalName, styles.icon) || (
-                  <img className={styles.icon} src={pageSvgUrl} alt="" />
-                ))}
-              {page && idx ? (
-                <input
-                  aria-label={intl.formatMessage(messages.changeItemName)}
-                  className={classnames(styles.input, {
-                    [styles.azureFunctionNameInput]: isAzureFunction
-                  })}
-                  maxLength={maxInputLength}
-                  value={page ? page.title : azureFunctionName!.title}
-                  onChange={e => {
-                    if (handleInputChange && idx) {
-                      handleInputChange(e.target.value, idx - 1, true);
-                    }
-                  }}
-                  onBlur={e => {
-                    if (handleInputChange && idx && page && page.isValidTitle===false) {
-                      handleInputChange(validValue, idx - 1, false);
-                    }else{
-                      handleInputChange(e.target.value, idx - 1, false);
-                    }
-                    if (page.isValidTitle) setValidValue(page.title);
-                  }}
-                  autoFocus={page.isDirty}
-                  disabled={selectedPages.filter(selPage => selPage.title!==page.title && selPage.isValidTitle===false).length>0}
-                  ref={inputRef}
-                />
-              ) : (
+              {idx && (
                 <input
                   className={classnames(
                     styles.disabledInput,
@@ -186,21 +115,8 @@ const DraggableSidebarItem = ({
               )}
             </div>
           </div>
-          {page && page.isValidTitle===false && page.error && (
-            <div
-              className={classnames({
-                [styles.errorTextContainer]:
-                  true,
-                [styles.textContainer]: true,
-                [styles.largeIndentContainer]: false
-              })}
-            >
-              {intl.formatMessage(page.error)}
-            </div>
-          )}
 
         </div>
-        {(totalCount !== undefined ? totalCount > 1 : true) && (
           <CloseSVG
             tabIndex={0}
             onClick={handleCloseOnClick}
@@ -208,7 +124,6 @@ const DraggableSidebarItem = ({
             className={styles.cancelIcon}
             aria-label={intl.formatMessage(messages.deleteItem)}
           />
-        )}
       </div>
     </div>
   );
