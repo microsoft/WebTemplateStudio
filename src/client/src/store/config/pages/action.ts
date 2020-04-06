8376498f26d21060e85ee3a6354468089d80d860
japarisi <japarisivaldes@gmail.com>
2020-04-06 16:42:07 +0200

    fix lint test

--- src/client/src/store/config/pages/action.ts
+++ src/client/src/store/config/pages/action.ts
@@ -1,7 +1,6 @@
 import { ISetVisitedPageAction, IResetVisitedPageAction, ISetPageAction, ISetDetails, IPageOptionsActionType } from "./model";
 import { WIZARD_INFO_TYPEKEYS, WIZARD_CONTENT_TYPEKEYS } from "../../typeKeys";
 import { IOption } from "../../../types/option";
-import { IApiTemplateInfo } from "../../../types/apiTemplateInfo";
 
 const setVisitedWizardPageAction = (route: string): ISetVisitedPageAction => ({
   type: WIZARD_INFO_TYPEKEYS.SET_VISITED_WIZARD_PAGE,
@@ -36,39 +35,6 @@ const getPagesOptionsSuccessAction = (
   payload: pagesOptions,
   type: WIZARD_CONTENT_TYPEKEYS.GET_PAGES_OPTIONS_SUCCESS
 });
-
-function getApiTemplateInfoFromJson(items: any[]): IApiTemplateInfo[] {
-  const listItems = items.map<IApiTemplateInfo>(val => ({
-    displayName: val.name,
-    licenses: val.licenses,
-    longDescription: val.richDescription,
-    name: val.templateId,
-    position: val.displayOrder,
-    selected: false,
-    summary: val.description,
-    svgUrl: val.icon,
-    tags: val.tags,
-    defaultName: val.defaultName,
-    author: val.author
-  })).sort((a: IApiTemplateInfo, b: IApiTemplateInfo) => a.position - b.position);
-
-  return listItems;
-}
-
-function getOptionalFromApiTemplateInfo(items: IApiTemplateInfo[]): IOption[] {
-  return items.map<IOption>(val => ({
-    body: val.summary,
-    internalName: val.name,
-    licenses: val.licenses,
-    longDescription: val.longDescription,
-    selected: val.selected,
-    svgUrl: "",
-    title: val.displayName,
-    defaultName: val.defaultName,
-    isValidTitle: true,
-    author: val.author
-  }));
-}
 
 export {
   getPagesOptionsSuccessAction,
