f01d8ba8f045d27ea0e7e6671e11c52dc0cb5187
japarisi <japarisivaldes@gmail.com>
2020-04-21 17:40:24 +0200

    add Bootstrap List and Grid Template

--- templates/Web/Pages/Vue.Grid/src/views/VueGrid.vue
+++ templates/Web/Pages/Vue.Grid/src/views/VueGrid.vue
@@ -12,7 +12,7 @@
 
       <div class="container">
         <div class="row justify-content-center py-5">
-          <h1>Bootstrap VueGrid Template</h1>
+          <h1>Bootstrap Grid Template</h1>
         </div>
 
         <div class="row justify-content-around text-center pb-5">
