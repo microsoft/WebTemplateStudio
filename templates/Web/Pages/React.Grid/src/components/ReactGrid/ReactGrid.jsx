15d5a10aba18cdb4efe0f48c5778cd2f02ad386a
japarisi <japarisivaldes@gmail.com>
2020-04-23 13:34:57 +0200

    clean unused imports

--- templates/Web/Pages/React.Grid/src/components/ReactGrid/ReactGrid.jsx
+++ templates/Web/Pages/React.Grid/src/components/ReactGrid/ReactGrid.jsx
@@ -1,7 +1,5 @@
 ﻿import React, { useState } from "react";
-import classnames from "classnames";
 import WarningMessage from "../WarningMessage";
-import styles from "./styles.module.css";
 import CONSTANTS from "../../constants";
 import GridItem from "./GridItem";
 
