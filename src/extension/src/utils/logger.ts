53a9592cde9acf535a46dd657886dc6f3c549fa1
dgomezc <v-davgom@microsoft.com>
2020-04-23 16:06:51 +0200

    Create loggerModule in extension code.

--- src/extension/src/utils/logger.ts
+++ src/extension/src/utils/logger.ts
@@ -1,15 +1,8 @@
 import * as vscode from "vscode";
-import { WizardServant, IPayloadResponse } from "../wizardServant";
-import { ExtensionCommand } from "../constants";
 import path = require("path");
 import log4js = require("log4js");
-type LogLevel = "trace" | "debug" | "info" | "warn" | "error" | "fatal";
-type LogSource = "WIZARD" | "EXTENSION" | "CORE";
-type ILoggingPayload = {
-  level: LogLevel;
-  data: string;
-};
-
+export type LogLevel = "trace" | "debug" | "info" | "warn" | "error" | "fatal";
+export type LogSource = "WIZARD" | "EXTENSION" | "CORE";
 export const LOG_FILE = path.join(__dirname, "../../logs", "wts.log");
 const OUTPUT_CHANNEL_DEFAULT = "Web Template Studio";
 
@@ -19,56 +12,40 @@ log4js.configure({
       type: "file",
       filename: LOG_FILE,
       pattern: ".yyyy-MM-dd",
-      daysToKeep: 5
-    }
+      daysToKeep: 5,
+    },
   },
   categories: {
     default: {
       appenders: ["webtemplatestudio"],
-      level: "all"
-    }
-  }
+      level: "all",
+    },
+  },
 });
 
-export class Logger extends WizardServant {
-  clientCommandMap: Map<
-    ExtensionCommand,
-    (message: any) => Promise<IPayloadResponse>
-  > = new Map([[ExtensionCommand.Log, Logger.receiveLogfromWizard]]);
+export class Logger {
   public static outputChannel: vscode.OutputChannel;
   private static logger: log4js.Logger = log4js.getLogger();
+
   public static initializeOutputChannel(extensionName: string): void {
     if (Logger.outputChannel === undefined) {
       Logger.outputChannel = vscode.window.createOutputChannel(extensionName);
     }
     Logger.appendLog("EXTENSION", "info", "Launched");
   }
-  public static appendLog(
-    source: LogSource,
-    level: LogLevel,
-    data: string
-  ): void {
+
+  public static appendLog(source: LogSource, level: LogLevel, data: string): void {
     if (Logger.outputChannel === undefined) {
       Logger.initializeOutputChannel(OUTPUT_CHANNEL_DEFAULT);
     }
     Logger.logger[level]("[", source, "] ", data);
-    Logger.outputChannel.appendLine(
-      "[".concat(new Date().toLocaleString(), "]", "[", source, "] ", data)
-    );
+    Logger.outputChannel.appendLine("[".concat(new Date().toLocaleString(), "]", "[", source, "] ", data));
   }
+
   public static display(): void {
     if (Logger.outputChannel === undefined) {
       Logger.initializeOutputChannel(OUTPUT_CHANNEL_DEFAULT);
     }
     Logger.outputChannel.show(true);
-  }
-  private static async receiveLogfromWizard(
-    message: ILoggingPayload
-  ): Promise<IPayloadResponse> {
-    Logger.appendLog("WIZARD", message.level, message.data);
-    Logger.display();
-    return {
-      payload: null
-    };
   }
 }
