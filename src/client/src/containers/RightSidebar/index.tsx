494d619df27846175cd1f42f2d723341c36b3d51
japarisi <japarisivaldes@gmail.com>
2020-05-13 11:55:50 +0200

    fix show right side bar on new project

--- src/client/src/containers/RightSidebar/index.tsx
+++ src/client/src/containers/RightSidebar/index.tsx
@@ -2,11 +2,9 @@ import classnames from "classnames";
 import * as React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { injectIntl, InjectedIntlProps } from "react-intl";
-
 import ServicesList from "./ServicesList";
 import About from "./About";
 import SelectPages from "./SelectPages";
-
 import styles from "./styles.module.css";
 import buttonStyles from "../../css/buttonStyles.module.css";
 import {
@@ -26,31 +24,27 @@ type Props = InjectedIntlProps;
 
 const RightSidebar = (props: Props)=>{
   const [ isSidebarOpen, setIsSiderbarOpen ] = React.useState(true);
-  const [ isSidebarUserControlled, setIsSidebarUserControlled ] = React.useState(false);
-
   const hasServices: boolean = useSelector(hasServicesSelector);
   const wizardRoutes = useSelector((state: AppState) => state.navigation.routes);
   const selectedRoute = useSelector((state: AppState) => state.navigation.routes.selected);
 
   const { intl } = props;
   const { formatMessage } = intl;
-
   const dispatch = useDispatch();
 
   React.useEffect(()=>{
-    if (wizardRoutes.isVisited["/SelectPages"]===true &&
-    !isSidebarUserControlled)
+    if (selectedRoute === ROUTES.NEW_PROJECT || 
+      selectedRoute === ROUTES.REVIEW_AND_GENERATE)
       setIsSiderbarOpen(true);
   },[wizardRoutes]);
 
-  const sidebarToggleClickHandler = () => {
+  const showHideMenu = () => {
     setIsSiderbarOpen(!isSidebarOpen);
-    setIsSidebarUserControlled(true);
   };
 
   const cancelKeyDownHandler = (event: React.KeyboardEvent<SVGSVGElement>) => {
     if (event.key === KEY_EVENTS.ENTER || event.key === KEY_EVENTS.SPACE) {
-      sidebarToggleClickHandler();
+      showHideMenu();
     }
   };
 
@@ -61,8 +55,9 @@ const RightSidebar = (props: Props)=>{
       <button
         tabIndex={0}
         className={styles.hamburgerButton}
-        onClick={sidebarToggleClickHandler}
-        aria-label={intl.formatMessage(messages.openSideBar)}
+        onClick={showHideMenu}
+        aria-label={intl.formatMessage(messages.showAriaLabel)}
+        title={intl.formatMessage(messages.showIcon)}
       >
         <div className={styles.hamburgerLine} />
         <div className={styles.hamburgerLine} />
@@ -73,22 +68,17 @@ const RightSidebar = (props: Props)=>{
     {(isSidebarOpen || selectedRoute === ROUTES.REVIEW_AND_GENERATE) && (
       <div
         role="complementary" id="dvRightSideBar"
-        className={classnames(styles.container, styles.rightViewCropped, {
-          [styles.rightViewCroppedSummaryPage]:
-          selectedRoute === ROUTES.REVIEW_AND_GENERATE
-        })}
+        className={classnames(styles.container, styles.rightViewCropped)}
       >
       <div className={styles.summaryContainer} id="dvSummaryContainer">
-        {selectedRoute !== ROUTES.REVIEW_AND_GENERATE && selectedRoute !== ROUTES.NEW_PROJECT && (
-          <Cancel
-            tabIndex={0}
-            className={styles.icon}
-            onClick={sidebarToggleClickHandler}
-            onKeyDown={cancelKeyDownHandler}
-            aria-label={intl.formatMessage(messages.closeSideBar)}
-          />
-        )}
-
+        <Cancel
+          tabIndex={0}
+          className={classnames(styles.icon,{[styles.iconHide]: selectedRoute === ROUTES.REVIEW_AND_GENERATE || selectedRoute === ROUTES.NEW_PROJECT})}
+          onClick={showHideMenu}
+          onKeyDown={cancelKeyDownHandler}
+          aria-label={intl.formatMessage(messages.hideAriaLabel)}
+          title={intl.formatMessage(messages.hideIcon)}
+        />
         <ProjectDetails/>
         <SelectFrameworks/>
         <SelectPages pathname={selectedRoute}/>
