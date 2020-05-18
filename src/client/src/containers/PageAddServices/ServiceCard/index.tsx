1e6c0ea225050c98c5ad81d00fe6c613571b92e1
dgomezc <v-davgom@microsoft.com>
2020-05-18 17:21:17 +0200

    Move CardBody code to ServiceCard

--- src/client/src/containers/PageAddServices/ServiceCard/index.tsx
+++ src/client/src/containers/PageAddServices/ServiceCard/index.tsx
@@ -8,14 +8,15 @@ import buttonStyles from "../../../css/buttonStyles.module.css";
 import styles from "./styles.module.css";
 import { IOption } from "../../../types/option";
 import { getSvg } from "../../../utils/getSvgUrl";
-import CardBody from "../../../components/CardBody";
 import { KEY_EVENTS, ROUTES, WIZARD_CONTENT_INTERNAL_NAMES } from "../../../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
 import { setPageWizardPageAction, setDetailPageAction } from "../../../store/navigation/routes/action";
 import messages from "./messages";
-import { getServices, hasSelectedService } from "../../../store/userSelection/services/servicesSelector";
+import { hasSelectedService } from "../../../store/userSelection/services/servicesSelector";
 import { isLoggedInSelector } from "../../../store/config/azure/selector";
 import { AppState } from "../../../store/combineReducers";
+import { ReactComponent as PriceSVG } from "../../../assets/money.svg";
+import { ReactComponent as TimeSVG } from "../../../assets/timer.svg";
 
 interface IProps {
   option: IOption;
@@ -34,8 +35,8 @@ export const ServiceCard = (props: Props) => {
     event.stopPropagation();
     dispatch(setPageWizardPageAction(ROUTES.PAGE_DETAILS));
     dispatch(setDetailPageAction(option, false, ROUTES.ADD_SERVICES));
-  }; 
-  
+  };
+
   const showDetailIfPressEnterKey = (event: React.KeyboardEvent<HTMLAnchorElement>) => {
     event.stopPropagation();
     if (event.key === KEY_EVENTS.ENTER) {
@@ -44,64 +45,61 @@ export const ServiceCard = (props: Props) => {
     }
   };
 
-  const openCloudServiceModal = (option: IOption) => {
-    const modalOpeners = {
-      [WIZARD_CONTENT_INTERNAL_NAMES.COSMOS_DB]: () => dispatch(ModalActions.openCosmosDbModalAction()),
-      [WIZARD_CONTENT_INTERNAL_NAMES.APP_SERVICE]: ()=> dispatch(ModalActions.openAppServiceModalAction())
-    };
-    if (modalOpeners.hasOwnProperty(option.internalName)) {
-      modalOpeners[option.internalName]();
-    }
-    //return () => void(0);
-  }
-
-  const openLoginModal = (option: IOption) => {
-    dispatch(ModalActions.openAzureLoginModalAction(option.internalName));
-  }
+  const openModal = () => {
+    if(isLoggedIn) {
+      if(option.internalName === WIZARD_CONTENT_INTERNAL_NAMES.APP_SERVICE) {
+        dispatch(ModalActions.openAppServiceModalAction());
+      } else if (option.internalName === WIZARD_CONTENT_INTERNAL_NAMES.COSMOS_DB) {
+        dispatch(ModalActions.openCosmosDbModalAction());
+      }
+    } else {
+      dispatch(ModalActions.openAzureLoginModalAction(option.internalName));
+    }    
+  };
 
   return (
     <div className={styles.container}>
-    <div className={styles.loginContainer}>
-      <div className={styles.cardTitleContainer}>
-        {getSvg(option.internalName, styles.icon)}
-        <div className={styles.cardTitle}>{option.title}</div>
-      </div>
-      <div className={styles.cardContent}>
-        <div className={styles.cardBody}>
-          <CardBody
-            formattedBody={option.body as string}
-            expectedTime={option.expectedTime as string|undefined}
-            expectedPrice={option.expectedPrice as string|undefined}
-          />
+      <div className={styles.loginContainer}>
+        <div className={styles.cardTitleContainer}>
+          {getSvg(option.internalName, styles.icon)}
+          <div className={styles.cardTitle}>{option.title}</div>
         </div>
-        <div className={styles.selectionContainer}>
-          <a
-            tabIndex={0}
-            onClick={showDetails}
-            onKeyDown={showDetailIfPressEnterKey}
-            className={styles.details}>
+        <div className={styles.cardContent}>
+          <div className={styles.cardBody}>
+            <div className={styles.body}>
+              {option.expectedPrice && (
+                <div className={styles.expectedPrice}>
+                  {option.expectedPrice && <PriceSVG className={styles.svg} />}
+                  {option.expectedPrice && <div>{option.expectedPrice}</div>}
+                </div>
+              )}
+              {option.expectedTime && (
+                <div className={styles.expectedTime}>
+                  {option.expectedTime && <TimeSVG className={styles.svg} />}
+                  {option.expectedTime && <div>{option.expectedTime}</div>}
+                </div>
+              )}
+              <div className={styles.formattedBody}>{option.body}</div>
+            </div>
+          </div>
+          <div className={styles.selectionContainer}>
+            <a tabIndex={0} onClick={showDetails} onKeyDown={showDetailIfPressEnterKey} className={styles.details}>
               {formatMessage(messages.learnMore)}
-          </a>
-          <button
-            onClick={
-              isLoggedIn
-                ? () => openCloudServiceModal(option)
-                : () => openLoginModal(option)
-            }
-            className={classnames(
-              styles.signInButton,
-              buttonStyles.buttonHighlighted,
-              buttonStyles.buttonCursorPointer
-            )}
-            tabIndex={0}
-          >
-            {hasService 
-              ? formatMessage( messages.editResource) 
-              : formatMessage( messages.addToProject)}
-          </button>
+            </a>
+            <button
+              onClick={openModal}
+              className={classnames(
+                styles.signInButton,
+                buttonStyles.buttonHighlighted,
+                buttonStyles.buttonCursorPointer
+              )}
+              tabIndex={0}
+            >
+              {hasService ? formatMessage(messages.editResource) : formatMessage(messages.addToProject)}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
