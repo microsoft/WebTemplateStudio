bbb7984bda7078a19b7910dad60ce751ffcdcf1f
japarisi <japarisivaldes@gmail.com>
2020-05-27 10:39:42 +0200

    change vscode to @types/vscode and run test

--- src/extension/src/utils/extensions.ts
+++ src/extension/src/utils/extensions.ts
@@ -1,4 +1,4 @@
-const asyncForEach = async (array: Array<any>, callback: Function): Promise<any> => {
+const asyncForEach = async (array: Array<any>, callback: any): Promise<any> => {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array);
   }
