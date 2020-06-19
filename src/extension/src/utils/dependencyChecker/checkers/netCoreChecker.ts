e0680f07fd6bc9fdbaa31d2dc591ce42a33307a0
dgomezc <v-davgom@microsoft.com>
2020-06-19 12:47:06 +0200

    Add net core version checker

--- src/extension/src/utils/dependencyChecker/checkers/netCoreChecker.ts
+++ src/extension/src/utils/dependencyChecker/checkers/netCoreChecker.ts
@@ -1,6 +1,19 @@
+import util = require("util");
+import semver = require("semver");
 
 export default class NetCoreChecker implements IDependencyChecker {
+    private exec = util.promisify(require("child_process").exec);
+    private NETCORE_REGEX = RegExp("([0-9.]+)");
+    private NETCORE_REQUIREMENT = ">=3.1.x";
+
     public async hasDependency() {
-        return await new Promise<boolean>(() => true);
+        try {
+            const { stdout } = await this.exec("dotnet --version");
+            const version = stdout.match(this.NETCORE_REGEX)[1];
+            const result = semver.satisfies(version, this.NETCORE_REQUIREMENT);
+            return result;
+          } catch (err) {
+            return false;
+          }
     }
   }
