e372cbcf6afb5fcf5d933cfc89254b38e9bb3620
dgomezc <v-davgom@microsoft.com>
2020-06-23 12:22:15 +0200

    Update basic asp net template

--- templates/Web/Projects/AspNetDefault/server/Program.cs
+++ templates/Web/Projects/AspNetDefault/server/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace WTSWebApi
+namespace Param_RootNamespace_Pascal.WebApi
 {
     public class Program
     {
--- templates/Web/Projects/AspNetDefault/server/Startup.cs
+++ templates/Web/Projects/AspNetDefault/server/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace WTSWebApi
+namespace Param_RootNamespace_Pascal.WebApi
 {
     public class Startup
     {
