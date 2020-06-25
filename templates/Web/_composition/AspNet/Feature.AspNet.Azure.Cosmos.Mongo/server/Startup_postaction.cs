849d57fa9c40de769393127a1eef305ac9b087c8
dgomezc <v-davgom@microsoft.com>
2020-06-25 13:04:41 +0200

    Add cosmosDB compositions

---
+++ templates/Web/_composition/AspNet/Feature.AspNet.Azure.Cosmos.Mongo/server/Startup_postaction.cs
@@ -0,0 +1,30 @@
+using Microsoft.Extensions.DependencyInjection;
+//{[{
+using System.Security.Authentication;
+using MongoDB.Driver;
+//}]}
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            //{[{
+            services.AddSingleton<IMongoClient>(InitializeCosmosClientInstance());
+            //}]}
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+        }
+        //{[{
+
+        private IMongoClient InitializeCosmosClientInstance()
+        {
+            var connectionString = Configuration.GetConnectionString("cosmosDb");
+            var settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
+            settings.SslSettings = new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+            return new MongoClient(settings);
+        }
+        //}]}
+    }
