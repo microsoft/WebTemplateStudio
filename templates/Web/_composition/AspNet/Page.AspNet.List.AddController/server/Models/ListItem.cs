f7c84b441f928d60c73ff433f02e7afe2fa7a23b
dgomezc <v-davgom@microsoft.com>
2020-06-26 10:28:40 +0200

    Split ListItem model with postactions

--- templates/Web/_composition/AspNet/Page.AspNet.List.AddController/server/Models/ListItem.cs
+++ templates/Web/_composition/AspNet/Page.AspNet.List.AddController/server/Models/ListItem.cs
@@ -1,14 +1,11 @@
 using System;
-using Newtonsoft.Json;
 
 namespace Param_RootNamespace_Pascal.WebApi.Models
 {
     public class ListItem
     {
-        [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
-        
-        [JsonProperty(PropertyName = "text")]
+
         public string Text { get; set; }
     }
 }
