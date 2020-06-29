02bcc77bd1a1701d4b6b3f03ab84a8a136055b53
japarisi <japarisivaldes@gmail.com>
2020-06-29 11:26:29 +0200

    add licenses to templates

--- src/client/src/containers/ViewLicensesModal/Licenses/index.tsx
+++ src/client/src/containers/ViewLicensesModal/Licenses/index.tsx
@@ -8,58 +8,25 @@ import {
 import { ILicenseObject } from "../../../types/license";
 
 import styles from "./styles.module.css";
-import { injectIntl, InjectedIntlProps } from "react-intl";
+import { injectIntl } from "react-intl";
 
-import { AppState } from "../../../store/combineReducers";
-import { IVisitedPages, getIsVisitedRoutesSelector } from "../../../store/config/config/wizardNavigationSelector";
 import { getGenerationData } from "../../../store/userSelection/app/selector";
 import { getAllLicenses } from "../../../utils/extensionService/extensionService";
 import { AppContext } from "../../../AppContext";
 
-interface IStateProps {
-  frameworkLicenses: string[];
-  pageLicenses: ILicenseObject[];
-  isVisited: IVisitedPages;
-}
-
-type Props = IStateProps & InjectedIntlProps;
-
-const Licenses = ({
-  frameworkLicenses,
-  pageLicenses,
-  isVisited
-}: Props) => {
+const Licenses = () => {
   const { vscode } = React.useContext(AppContext);
   const generationData = useSelector(getGenerationData);
-  const [licenses, setLicenses] = React.useState("pte");
-  getAllLicenses(generationData, vscode).then(()=>{
-    setLicenses("resolved");
-  });
+  const [licenses, setLicenses] = React.useState<ILicenseObject[]>([]);
+  React.useEffect(()=>{
+    getAllLicenses(generationData, vscode).then((event)=>{
+      setLicenses(event.data.payload.licenses);
+    });
+  },[]);
 
-  const LinkRenderer = (props: any) => {
-    return (
-      <a
-        className={styles.licenseButton}
-        href={String(props.href)}
-        target={"_blank"}
-        rel="noreferrer noopener"
-      >
-        {props.children} 
-        -- {licenses}
-      </a>
-    );
-  };
   return (
     <div className={styles.container}>
-      {frameworkLicenses.map((license: string, idx: number) => (
-        <ReactMarkdown
-          key={`${license} + ${idx}`}
-          source={license}
-          renderers={{ link: LinkRenderer }}
-        />
-      ))}
-      {isVisited.showPages &&
-        pageLicenses.map((license: ILicenseObject) => (
+      {licenses.map((license: ILicenseObject) => (
           <p key={license.url}>
             <a
               className={styles.licenseButton}
@@ -76,12 +43,4 @@ const Licenses = ({
   );
 };
 
-const mapStateToProps = (state: AppState) => ({
-  frameworkLicenses: getFrameworkLicensesSelector(state),
-  isVisited: getIsVisitedRoutesSelector(state),
-  pageLicenses: getPageLicencesSelector(state)
-});
-
-export default connect(
-  mapStateToProps
-)(injectIntl(Licenses));
+export default (injectIntl(Licenses));
