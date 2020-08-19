af2cc3ac00c341e437db4c847bc76f37ce74abd7
japarisi <japarisivaldes@gmail.com>
2020-08-19 16:52:59 +0200

    set optional closeModal parameter

--- src/client/src/components/Modal/index.tsx
+++ src/client/src/components/Modal/index.tsx
@@ -5,7 +5,7 @@ import styles from "./styles.module.css";
 import classnames from "classnames";
 
 interface IProps {
-  closeModal: () => any;
+  closeModal: () => any | null;
   isModalOpen: boolean;
 }
 
