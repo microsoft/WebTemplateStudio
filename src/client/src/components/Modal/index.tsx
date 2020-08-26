65314aee5c2be25cbafaed51878ef6a10a368f9f
japarisi <japarisivaldes@gmail.com>
2020-08-26 11:56:05 +0200

    delete unused elements

--- src/client/src/components/Modal/index.tsx
+++ src/client/src/components/Modal/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { ModalType, NAVIGATION_MODAL_TYPES } from "../../store/navigation/typeKeys";
-//import ReactDOM from "react-dom";
 import styles from "./styles.module.css";
 import classnames from "classnames";
 
@@ -8,31 +7,6 @@ interface IProps {
   closeModal?: any;
   isModalOpen: boolean;
 }
-
-/*class ModalClass extends React.Component {
-  el: HTMLDivElement;
-  modalRoot: HTMLDivElement;
-  constructor(props: any) {
-    super(props);
-    this.el = document.getElementById('modal-el') as HTMLDivElement;
-    this.modalRoot = document.getElementById('modal-root') as HTMLDivElement;
-  }
-
-  componentDidMount() {
-    this.modalRoot.appendChild(this.el);
-  }
-
-  componentWillUnmount() {
-    this.modalRoot.removeChild(this.el);
-  }
-
-  render() {
-    return ReactDOM.createPortal(
-      this.props.children,
-      this.el
-    );
-  }
-}*/
 
 const asModal = <P extends object>(
   WrappedComponent: React.ComponentType<P>,
