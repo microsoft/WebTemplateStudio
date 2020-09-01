85c9b43faf23a5a84ae52978f67c5e584436bb21
dgomezc <v-davgom@microsoft.com>
2020-09-01 18:16:10 +0200

    Update package.json to all frameworks

--- templates/Web/_composition/Flask/Feature.Flask.Azure.Cosmos.SQL/backend/sql/settings.py
+++ templates/Web/_composition/Flask/Feature.Flask.Azure.Cosmos.SQL/backend/sql/settings.py
@@ -3,7 +3,7 @@ from os.path import join, dirname, realpath
 from dotenv import load_dotenv
 
 # Create .env file path.
-dotenv_path = join(dirname(dirname(dirname(realpath(__file__)))), '.env')
+dotenv_path = join(dirname(dirname(realpath(__file__))), '.env')
 
 # Load file from the above created path.
 load_dotenv(dotenv_path)
