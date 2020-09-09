e0eeb51f6875b7bd42275e3c62696873e7acda67
dgomezc <v-davgom@microsoft.com>
2020-09-09 09:22:53 +0200

    Add local string to commands to build replace

--- src/extension/src/extension.ts
+++ src/extension/src/extension.ts
@@ -6,13 +6,13 @@ import { Platform } from "./constants/constants";
 export function activate(context: vscode.ExtensionContext): void {
   context.subscriptions.push(
     vscode.commands.registerCommand(
-      "webTemplateStudioExtension.wizardLaunch",
+      "webTemplateStudioExtension.wizardLaunch.local",
       async () => {
         Controller.getInstance(context, Platform.Web);
       }
     ),
     vscode.commands.registerCommand(
-      "webTemplateStudioExtension.deployApp",
+      "webTemplateStudioExtension.deployApp.local",
       async () => {
         Deploy.getInstance(context);
       }
