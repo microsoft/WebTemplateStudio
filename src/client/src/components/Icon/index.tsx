ecb7aa3a07c88f423b9ab0cc5bd51499d10f4de6
Judith de la Fuente <jud.yureawen@gmail.com>
2020-09-29 11:18:08 +0200

    icon component slightly modified - small style - empty image

--- src/client/src/components/Icon/index.tsx
+++ src/client/src/components/Icon/index.tsx
@@ -4,19 +4,18 @@ import styles from "./styles.module.css";
 
 export interface IIconProps {
   name: string;
-  svgUrl?: string;
   svgBase64?: string;
+  small?: boolean;
 }
 
-const Icon = ({ name, svgUrl, svgBase64 }: IIconProps) => {
+const Icon = ({ name, svgBase64, small = false }: IIconProps) => {
   return (
     <>
       {svgBase64 ? (
-        <img className={styles.icon} alt={name} src={"data:image/svg+xml;base64," + svgBase64} />
+        <img className={small ? styles.small : styles.icon} alt={name} src={"data:image/svg+xml;base64," + svgBase64} />
       ) : (
-        svgUrl && <img className={styles.icon} alt={name} src={svgUrl} />
+        <img className={styles.icon} />
       )}
-      {/* //TODO Add missing image. Cancel SVG? duplicate and call default  */}
     </>
   );
 };
