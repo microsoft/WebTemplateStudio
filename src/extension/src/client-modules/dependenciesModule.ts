0e29ce5207d663438976a0adc382737bbde10390
dgomezc <v-davgom@microsoft.com>
2020-10-15 17:32:52 +0200

    Remove unnecessary code

--- src/extension/src/client-modules/dependenciesModule.ts
+++ src/extension/src/client-modules/dependenciesModule.ts
@@ -1,26 +1,12 @@
 import { WizardServant, IPayloadResponse } from "../wizardServant";
 import { EXTENSION_COMMANDS } from "../constants/commands";
-import RequirementsService from "../utils/requirements/requirementsService";
 import PackageVersionService from "../utils/packageVersions/PackageVersionService";
 
 export class DependenciesModule extends WizardServant {
-  private requirementsService = new RequirementsService();
   private packageVersionService = new PackageVersionService();
   clientCommandMap: Map<EXTENSION_COMMANDS, (message: any) => Promise<IPayloadResponse>> = new Map([
-    [EXTENSION_COMMANDS.GET_DEPENDENCY_INFO, this.requirementIsInstalled],
     [EXTENSION_COMMANDS.GET_LATEST_VERSION, this.getLatestVersion],
   ]);
-
-  async requirementIsInstalled(message: any): Promise<IPayloadResponse> {
-    const dependency = message.payload.dependency as string;
-    const installed = await this.requirementsService.isInstalled(dependency, ">=20.x");
-    return {
-      payload: {
-        dependency,
-        installed,
-      },
-    };
-  }
 
   public async getLatestVersion(message: any): Promise<any> {
     const { name, source } = message.payload.checkVersionPackage;
