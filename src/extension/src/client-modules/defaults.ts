1afd4ec25d283cdfcd93f98f5917bb2e36cbd1b2
Judith de la Fuente <jud.yureawen@gmail.com>
2020-10-27 12:35:44 +0100

    check path is valid and log error if not

--- src/extension/src/client-modules/defaults.ts
+++ src/extension/src/client-modules/defaults.ts
@@ -1,29 +1,19 @@
 import { WizardServant, IPayloadResponse } from "../wizardServant";
-import { PROJECT_NAME_VALIDATION_LIMIT, DEFAULT_PROJECT_NAME } from "../constants/constants";
 import * as vscode from "vscode";
 import * as os from "os";
 import * as fs from "fs";
 import * as path from "path";
 import { EXTENSION_COMMANDS } from "../constants/commands";
 import { CLI_SETTINGS } from "../constants/cli";
+import { Logger } from "../utils/logger";
 
 export class Defaults extends WizardServant {
   clientCommandMap: Map<EXTENSION_COMMANDS, (message: any) => Promise<IPayloadResponse>> = new Map([
-    [EXTENSION_COMMANDS.GET_PROJECT_NAME, this.getProjectName],
     [EXTENSION_COMMANDS.GET_OUTPUT_PATH_FROM_CONFIG, this.getOutputPathFromConfig],
-    [EXTENSION_COMMANDS.BROWSE_NEW_OUTPUT_PATH, this.browseNewOutputPath],
+        [EXTENSION_COMMANDS.BROWSE_NEW_OUTPUT_PATH, this.browseNewOutputPath],
   ]);
 
-  public async getProjectName(): Promise<IPayloadResponse> {
-    const outputPath = this.getDefaultProjectPath();
-    const projectName = await this.inferProjectName(outputPath);
-    return {
-      payload: {
-        projectName,
-      },
-    };
-  }
-
+  
   public async getOutputPathFromConfig(): Promise<IPayloadResponse> {
     const outputPath = this.getDefaultProjectPath();
     return {
@@ -45,25 +35,19 @@ export class Defaults extends WizardServant {
     });
   }
 
-  private getDefaultProjectPath(): string {
-    const projectPath = vscode.workspace.getConfiguration().get<string>("wts.changeSaveToLocation");
-    return (projectPath !== undefined && projectPath !== "")
-      ? projectPath
-      : os.homedir();
-  }
+  private getDefaultProjectPath(logPathError?: boolean): string {
+    let projectPath = vscode.workspace.getConfiguration().get<string>("wts.changeSaveToLocation");
+    Logger.appendLog("EXTENSION", "error", `Testing what´s stored ... ${projectPath}`);
 
-  private async inferProjectName(outputPath: string): Promise<string> {
-    let projectName = DEFAULT_PROJECT_NAME;
-    let count = 1;
 
-    while (fs.existsSync(path.join(outputPath, projectName)) && count <= PROJECT_NAME_VALIDATION_LIMIT) {
-      projectName = `${DEFAULT_PROJECT_NAME}${count}`;
-      count++;
+    //if path is wrong return default path
+    if (projectPath !== undefined && !fs.existsSync(path.join(projectPath, ""))) {
+      logPathError?
+      Logger.appendLog("EXTENSION", "error", `The configured default path '${projectPath}' does not seem to be valid.`):"";
+      projectPath = os.homedir();
     }
-    if (count > PROJECT_NAME_VALIDATION_LIMIT) {
-      projectName = "";
-    }
-    return projectName;
+
+    return projectPath !== undefined && projectPath !== "" ? projectPath : os.homedir();
   }
 
   private getOutputPath(path?: vscode.Uri[]): string | undefined {
