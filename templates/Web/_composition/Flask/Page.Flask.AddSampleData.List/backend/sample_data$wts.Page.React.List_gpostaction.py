3e673b092647dadb8e8504373c9baddf12e4fef6
dgomezc <v-davgom@microsoft.com>
2020-11-23 14:31:45 +0100

    Refactor delete list items and sample data

--- templates/Web/_composition/Flask/Page.Flask.AddSampleData.List/backend/sample_data$wts.Page.React.List_gpostaction.py
+++ templates/Web/_composition/Flask/Page.Flask.AddSampleData.List/backend/sample_data$wts.Page.React.List_gpostaction.py
@@ -10,16 +10,14 @@ short_lorem_ipsum = 'Lorem id sint aliqua tempor tempor sit. Ad dolor dolor ut n
 # This class holds sample data used by some generated pages to show how they can be used.
 # TODO Web Template Studio: Delete this file once your app is using real data.
 //{[{
-sample_data['list_text_assets'] = {
-    'list_items': [
-        {
-            'id': str(uuid.uuid4()),
-            'text': short_lorem_ipsum
-        },
-        {
-            'id': str(uuid.uuid4()),
-            'text': short_lorem_ipsum
-        }
-    ],
-}
+sample_data['list_text_assets'] = [
+    {
+        'id': str(uuid.uuid4()),
+        'text': short_lorem_ipsum
+    },
+    {
+        'id': str(uuid.uuid4()),
+        'text': short_lorem_ipsum
+    }
+]
 //}]}
--- templates/Web/_composition/Flask/Page.Flask.List.AddRoutes/backend/server_postaction.py
+++ templates/Web/_composition/Flask/Page.Flask.List.AddRoutes/backend/server_postaction.py
@@ -19,25 +19,22 @@ app = Flask(__name__, static_folder='build')
 # List Endpoints
 @app.route(constants.ENDPOINT_LIST)
 def get_list():
-    return jsonify(sample_data['list_text_assets']['list_items'])
+    return jsonify(sample_data['list_text_assets'])
 
 @app.route(constants.ENDPOINT_LIST, methods = ['POST'])
 def add_list_item():
     data = request.get_json()
     list_item = {'id':  str(uuid.uuid4()), 'text': data['text']}
-    sample_data['list_text_assets']['list_items'].insert(0, list_item)
+    sample_data['list_text_assets'].insert(0, list_item)
     json_response = jsonify(list_item)
     return make_response(json_response, constants.HTTP_STATUS_201_CREATED)
 
 @app.route(constants.ENDPOINT_LIST + '/<string:id>', methods=['DELETE'])
 def delete_list_item(id):
-    list_items_to_remove = [list_item for list_item in sample_data['list_text_assets']['list_items'] if list_item['id'] == id]
-    if not list_items_to_remove:
+    item_to_remove = next((item for item in sample_data['list_text_assets'] if item["id"] == id), None)
+    if item_to_remove is None:
         json_response = jsonify({'error': 'Could not find an item with the given id'})
         return make_response(json_response, constants.HTTP_STATUS_404_NOT_FOUND)
-    if len(list_items_to_remove) > 1:
-        json_response = jsonify({'error': 'More than one list items found with the same id'})
-        return make_response(json_response, constants.HTTP_STATUS_500_INTERNAL_SERVER_ERROR)
-    sample_data['list_text_assets']['list_items'] = [list_item for list_item in sample_data['list_text_assets']['list_items'] if list_item['id'] != id]
+    sample_data['list_text_assets'].remove(item_to_remove)
     return jsonify({'id': id, 'text': 'This comment was deleted'})
 //}]}
