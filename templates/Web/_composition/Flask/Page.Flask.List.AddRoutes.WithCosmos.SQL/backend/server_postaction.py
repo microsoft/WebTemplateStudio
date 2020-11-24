a31e02e82991a7d175e9ebf81eb6b69099a31c25
dgomezc <v-davgom@microsoft.com>
2020-11-24 17:24:47 +0100

    Fix sql server postaction

--- templates/Web/_composition/Flask/Page.Flask.List.AddRoutes.WithCosmos.SQL/backend/server_postaction.py
+++ templates/Web/_composition/Flask/Page.Flask.List.AddRoutes.WithCosmos.SQL/backend/server_postaction.py
@@ -11,19 +11,19 @@ app = flask.Flask(__name__, static_folder='build')
 # List Endpoints
 @app.route(constants.ENDPOINT_LIST)
 def get_list():
-    return jsonify(sql.service.get())
+    return flask.jsonify(sql.service.get())
 
 @app.route(constants.ENDPOINT_LIST, methods=['POST'])
 def add_list_item():
-    json_response = jsonify(sql.service.create())
-    return make_response(json_response, constants.HTTP_STATUS_201_CREATED)
+    json_response = flask.jsonify(sql.service.create())
+    return flask.make_response(json_response, constants.HTTP_STATUS_201_CREATED)
 
 @app.route(constants.ENDPOINT_LIST + '/<id>', methods=['DELETE'])
 def delete_list_item(id):
     try:
-        removed_item = jsonify(sql.service.delete(id))
+        removed_item = flask.jsonify(sql.service.delete(id))
         return removed_item
     except Exception as ex:
-        err_response = jsonify({'error': str(ex)})
-        return make_response(err_response, constants.HTTP_STATUS_404_NOT_FOUND)
+        err_response = flask.jsonify({'error': str(ex)})
+        return flask.make_response(err_response, constants.HTTP_STATUS_404_NOT_FOUND)
 //}]}
