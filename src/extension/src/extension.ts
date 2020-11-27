bd15f2fa212aa3b9051cac2b2bd76676723722bc
Judith de la Fuente <jud.yureawen@gmail.com>
2020-11-27 11:24:13 +0100

    test pre-commit hook

--- src/extension/src/extension.ts
+++ src/extension/src/extension.ts
@@ -3,11 +3,13 @@ import { Controller } from "./controller";
 import { Deploy } from "./deploy";
 import { PLATFORM } from "./constants/constants";
 
+fdescribe
 export function activate(context: vscode.ExtensionContext): void {
   context.subscriptions.push(
     vscode.commands.registerCommand("webTemplateStudioExtension.wizardLaunch.local", async () => {
       //TODO: Modify this to support React Native
-      Controller.getInstance(context, PLATFORM.WEB);
+      //TODO: PLATFORM.REACTNATIVE || PLATFORM.WEB
+      Controller.getInstance(context, PLATFORM.REACTNATIVE);
     }),
     vscode.commands.registerCommand("webTemplateStudioExtension.deployApp.local", async () => {
       await Deploy.getInstance(context).deployProject();
