0463584a883c6743598285ab08b4e9463dde5aaa
Judith de la Fuente <jud.yureawen@gmail.com>
2020-11-27 11:41:35 +0100

    s

--- src/extension/src/extension.ts
+++ src/extension/src/extension.ts
@@ -3,8 +3,8 @@ import { Controller } from "./controller";
 import { Deploy } from "./deploy";
 import { PLATFORM } from "./constants/constants";
 
-fdescribe
-debugger
+fdescribe 
+
 export function activate(context: vscode.ExtensionContext): void {
   context.subscriptions.push(
     vscode.commands.registerCommand("webTemplateStudioExtension.wizardLaunch.local", async () => {
