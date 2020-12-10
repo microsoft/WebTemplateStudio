04335fe5685b4f2d48cc0cbb14063777e2151b94
Judith de la Fuente <jud.yureawen@gmail.com>
2020-12-10 16:46:31 +0100

    Update src/client/src/pages/SelectProjectTypePage/messages.ts
    change to same name WinTS has

--- src/client/src/pages/SelectProjectTypePage/messages.ts
+++ src/client/src/pages/SelectProjectTypePage/messages.ts
@@ -3,7 +3,7 @@ import { defineMessages } from "react-intl";
 const messages = defineMessages({
   header: {
     id: "selectProjectTypePage.header",
-    defaultMessage: "Select the type of application",
+    defaultMessage: "Select a project type",
   },
 });
 export default messages;
