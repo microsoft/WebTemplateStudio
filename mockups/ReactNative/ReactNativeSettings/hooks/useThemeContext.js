673c6d64d05dfd1be9b8c1d51d2859cac932d9a9
Judith de la Fuente <jud.yureawen@gmail.com>
2021-01-27 15:59:09 +0100

    * delete duplicated project

--- mockups/ReactNative/ReactNativeSettings/hooks/useThemeContext.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-
-import { ThemeContext } from "../context/ThemeProvider";
-
-export default function useThemeContext(){
-    return useContext(ThemeContext);
-}
--- mockups/ReactNative/ReactNativeSettings/themes/index.js
+++
@@ -1,5 +0,0 @@
-//TODO: custom basic themes 
-import dark from './dark';
-import light from './light';
-
-export default{ dark, light}
