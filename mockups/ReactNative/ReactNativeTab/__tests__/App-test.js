8571a75d176d86520aeaefa78d37d9d418189c9e
Judith de la Fuente <jud.yureawen@gmail.com>
2021-02-02 12:28:29 +0100

    * test fixed *there´s a warning yet

--- mockups/ReactNative/ReactNativeTab/__tests__/App-test.js
+++ mockups/ReactNative/ReactNativeTab/__tests__/App-test.js
@@ -9,6 +9,8 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.useFakeTimers();
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
