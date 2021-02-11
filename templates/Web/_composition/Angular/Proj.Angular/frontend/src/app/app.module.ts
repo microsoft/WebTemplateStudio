5026ff49130aca48b2b5b423a4fa2e7e0aaeee47
Judith de la Fuente <jud.yureawen@gmail.com>
2021-02-11 11:16:09 +0100

    * move Angular folder

---
+++ templates/Web/_composition/Angular/Proj.Angular/frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavBarComponent } from './app-shell/nav-bar/nav-bar.component';
+import { FooterComponent } from './app-shell/footer/footer.component';
+import { ServiceInterceptor } from './service.interceptor';
+
+@NgModule({
+  declarations: [AppComponent, NavBarComponent, FooterComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ServiceInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
