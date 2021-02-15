ff8ae4a43293e934a982b1b911eae105eeb8d441
Judith de la Fuente <jud.yureawen@gmail.com>
2021-02-15 13:40:13 +0100

    * update gitignore to:  revert accidentally removed REACT folder

---
+++ templates/Web/_composition/React/Project.React/frontend/src/components/Footer/Footer.js
@@ -0,0 +1,39 @@
+﻿import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className="container-fluid">
+        <div className="row justify-content-around">
+          <div className="col-8 col-md-5">
+            <h5 className={styles.title}>wts.ReactDefault</h5>
+            <p className={styles.description}>
+              This is placeholder text. Your web app description goes here.
+            </p>
+          </div>
+          <div className="col-2">
+            <ul className="list-unstyled">
+              <li>
+                <a className={styles.footerlink} href="/">
+                  Example Link
+                </a>
+              </li>
+              <li>
+                <a className={styles.footerlink} href="/">
+                  Example Link
+                </a>
+              </li>
+              <li>
+                <a className={styles.footerlink} href="/">
+                  Example Link
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
---
+++ templates/Web/_composition/React/Project.React/frontend/src/index.js
@@ -0,0 +1,18 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
