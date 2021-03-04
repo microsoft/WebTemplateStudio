45e0f4c6401a141b9ad574ad55a348cb66138581
Judith de la Fuente <jud.yureawen@gmail.com>
2021-03-04 19:38:43 +0100

    * copy base rnw project files * replace AppName
    -- -- -- -- -- -- -- -- -- -- -- --
    -- MISSING TabNavigator --
    -- -- -- -- -- -- -- -- -- -- -- --

---
+++ templates/RN/Projects/AddInitialProjectStructure/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+
+import TabNavigation from './navigation/TabNavigation';
+
+function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <TabNavigation />
+    </NavigationContainer>
+  );
+}
+
+export default App;
