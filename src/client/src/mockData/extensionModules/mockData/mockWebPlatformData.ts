a527a53c01dd07dec4c41a1da49f6dd8c5f398d5
Judith de la Fuente <jud.yureawen@gmail.com>
2021-03-09 21:46:25 +0100

    small update to change platform only once for mocked data

--- src/client/src/mockData/extensionModules/mockData/mockWebPlatformData.ts
+++ src/client/src/mockData/extensionModules/mockData/mockWebPlatformData.ts
@@ -1,8 +1,7 @@
-import { PLATFORM } from "../../../utils/constants/constants";
+import { mockedPlatform } from "../../mockConfig";
 import { backendImage, frontendImage, noImage, pageImage, serviceImage } from "./mockSvgData";
 
-// WTS DEV: Change mock data to change platform
-const platform = PLATFORM.WEB;
+const platform = mockedPlatform;
 
 const projectTypes = [
   {
