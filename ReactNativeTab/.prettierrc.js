c8b59cad04f8ab62c3063a1a9f05e3a6e2f1f732
Judith de la Fuente <jud.yureawen@gmail.com>
2021-03-12 18:38:56 +0100

    * remove JS rnw tabbed solution

--- ReactNativeTab/.prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  bracketSpacing: true,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-};
--- ReactNativeTab/src/screens/MasterDetail/Detail/Detail.style.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export const getStyles = (theme) =>
-  StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: theme.colors.background,
-    },
-  });
