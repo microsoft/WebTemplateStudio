7508e991f01b3be5661708a75c2dad653fd0390f
dgomezc <v-davgom@microsoft.com>
2021-03-17 16:34:19 +0100

    Add theme to BlankStack postaction

---
+++ templates/RN/_composition/Tabbed/Page.Blank.AddTheme/src/navigation/wts.ItemNameStack_postaction.tsx
@@ -0,0 +1,13 @@
+﻿import * as React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import wts.ItemName from '../screens/wts.ItemName/wts.ItemNameScreen';
+import {getScreenOptions} from './navigation.options';
+//{[{
+import {useTheme} from '../context/ThemeContext';
+//}]}
+const wts.ItemNameStack = (): JSX.Element => {
+  //{[{
+  const {theme} = useTheme();
+  //}]}
+  const screenOptions = getScreenOptions(/*{[{*/theme/*}]}*/);
+};
