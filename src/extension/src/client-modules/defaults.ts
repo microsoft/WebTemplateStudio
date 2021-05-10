b02b20a2d5bb9f19a618f7546ac1a0c3f0bae1df
Judith de la Fuente <jud.yureawen@gmail.com>
2021-05-11 00:14:33 +0200

    * rename var

--- src/extension/src/client-modules/defaults.ts
+++ src/extension/src/client-modules/defaults.ts
@@ -24,8 +24,8 @@ export class Defaults extends WizardServant {
   }
 
   public async browseNewOutputPath(message: any): Promise<IPayloadResponse> {
-    const defaultUri = vscode.Uri.file(message.payload.outputPath ?? this.getDefaultProjectPath());
-    const openDialogConfig = { canSelectFiles: false, canSelectFolders: true, canSelectMany: false, defaultUri: defaultUri };
+    const currentLocation = vscode.Uri.file(message.payload.outputPath ?? this.getDefaultProjectPath());
+    const openDialogConfig = { canSelectFiles: false, canSelectFolders: true, canSelectMany: false, defaultUri: currentLocation };
 
     return vscode.window.showOpenDialog(openDialogConfig).then((response) => {
       const outputPath = this.getOutputPath(response);
